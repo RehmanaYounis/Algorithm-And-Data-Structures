fba8af2adbdcbc0af24bc0cbc29361577873b81b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-03-20 23:18:41 -0400

    Time: 82 ms (59.97%), Space: 21.4 MB (79.57%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -12,14 +12,11 @@ class Solution(object):
         def LCA (root, p,q):
             if not root: return
             if (root.val == p.val or root.val== q.val) or (root.val>p.val and root.val<q.val):
-                print('yay',root.val, p.val, q.val)
                 return root
             elif root.val<p.val and root.val<q.val:
                 return LCA(root.right, p,q)
-                # print('left',l)
             elif root.val>p.val and root.val>q.val:
                 return LCA(root.left, p,q)
-                # print('right',r)
             else:
                 return root
         return LCA(root, p,q)
