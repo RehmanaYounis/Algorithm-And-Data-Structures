6b36609ae5bc8a74b752efc5862a053d716d9feb
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-05-16 15:45:51 -0400

    Time: 496 ms (49.13%), Space: 14.6 MB (5.35%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,51 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        friend_map ={}
+        hashmap={}
         for i in range (len(nums)):
-            if nums[i] not in friend_map:
-                friend_map[target-nums[i]]=i
+            if (nums[i]) not in hashmap.keys():
+                hashmap[target-nums[i]]=i
             else:
-                return [friend_map[nums[i]], i]
+                return (i, hashmap[nums[i]])
+        
+        # print(hashmap)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # for i in range (len(nums)):
+        #     if nums[i] not in friend_map:
+        #         friend_map[target-nums[i]]=i
+        #     else:
+        #         return [friend_map[nums[i]], i]
