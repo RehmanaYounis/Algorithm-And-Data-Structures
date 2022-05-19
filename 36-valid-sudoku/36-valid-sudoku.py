328b3c05d3077161bf12ce3dd9bb452ece493529
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-05-19 17:09:51 -0400

    Time: 92 ms (59.45%), Space: 13.8 MB (6.84%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        for i in range(9):
+            hashmap={}
+            for j in range(9):
+                val=board[i][j]
+                
+                if val in hashmap.keys() and val !='.':
+                    return False
+                else:
+                    hashmap[val]=True
+                    
+        for i in range(9):
+            hashmap={}
+            for j in range(9):
+                val=board[j][i]
+                if val in hashmap.keys() and val !='.':
+                    return False
+                else:
+                    hashmap[val]=True
+            
+        # print(" correct till now")
+        hashmap=collections.defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                val=board[i][j]
+                key=(i//3, j//3)
+                if val =='.':
+                    continue
+                if val in hashmap[key] :
+                    return False
+                else:
+                    hashmap[key].add(val)
+        return True
