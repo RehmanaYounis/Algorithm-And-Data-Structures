c2df90b9edc6cf4b014eca62ff9132c4a2e9bf0b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-06-06 17:36:07 -0400

    Time: 543 ms (5.97%), Space: 14.2 MB (18.12%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,74 +1,19 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        nums=s
-        hashmap=collections.defaultdict(int)
-        l=0
-        res = 0
-        curMax=0
-        curLen = 0
-        for r in range (len(s)):
-            hashmap[s[r]]+=1
-            curMax=max(hashmap.values())
-            curLen= r-l+1
-            while (r - l + 1)-max(hashmap.values()) > k:
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hashmap=defaultdict()
+        l=0;
+        maxLen=0; curLen=0;
+        for r in range(len(s)):
+            curLen=r-l+1
+            hashmap[s[r]]=1 + hashmap.get(s[r],0 )
+           
+            while (curLen- max(hashmap.values()) > k):
+                print('first', r, curLen, max(hashmap.values()) )
                 hashmap[s[l]]-=1
-                l+=1     
-            res=max(res, r-l + 1)
-            # r+=1
-        return res
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         ashmap=dict()
-#         count = 0
-#         l,r=0,0
-#         maxLen=0
-#         subLen=0
-#         for i in s:
-            
-            
-#             hashmap[i] = 1+ hashmap.get(i,0)
-            
-#             subLen= r-l+1
-#             while subLen - max(hashmap.values()) > k:
-#                 hashmap[s[l]]= hashmap[s[l]]-1
-#                 print('see',hashmap[s[l]])
-#                 l=l+1
-#                 print(hashmap)
+                print('hm', hashmap)
+                l+=1
+                curLen=r-l+1
                 
             
-            
-#             maxLen= max(maxLen, subLen)
-#             r=r+1
-            
-#         return(maxLen-1)
-        
-        
+            maxLen=max(maxLen, curLen)
+        return(maxLen)
