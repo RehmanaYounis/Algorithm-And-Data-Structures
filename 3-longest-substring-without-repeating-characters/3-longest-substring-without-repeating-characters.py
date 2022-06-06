878c8db6d5f47c29979e76e3d940ba745e2af42b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-06-06 12:51:47 -0400

    Time: 114 ms (36.50%), Space: 14 MB (49.30%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,10 +4,10 @@ class Solution:
         l=0; maxLen=0
         for r in range(len(s)):
             # print(s[r],'  ' ,hashmap)
-            if s[r] in hashmap:
-                while s[r] in hashmap:
-                    hashmap.pop(s[l])
-                    l+=1
+            # if s[r] in hashmap:
+            while s[r] in hashmap:
+                hashmap.pop(s[l])
+                l+=1
             curLen=(r-l+1)
             maxLen=max(maxLen, curLen)
             hashmap[s[r]]=r
