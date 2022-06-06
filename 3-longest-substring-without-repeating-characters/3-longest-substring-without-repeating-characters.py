849551ac2fadcb4c531001ad261c0180694e1f3d
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-06-06 12:51:21 -0400

    Time: 128 ms (29.86%), Space: 14 MB (49.30%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,97 +1,15 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        subSet=set()
-        l = 0
-        maxLen=0
-        
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hashmap=defaultdict();
+        l=0; maxLen=0
         for r in range(len(s)):
-            while s[r] in subSet:
-                subSet.remove(s[l])
-                l+=1
-            subSet.add(s[r])
-            maxLen=max(maxLen, r-l+1)
+            # print(s[r],'  ' ,hashmap)
+            if s[r] in hashmap:
+                while s[r] in hashmap:
+                    hashmap.pop(s[l])
+                    l+=1
+            curLen=(r-l+1)
+            maxLen=max(maxLen, curLen)
+            hashmap[s[r]]=r
         return maxLen
-                
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         maxcount = 0
-#         count = 0
-#         hashmap={}
-#         l,r= 0, 0
-#         while r < (len(s)):            
-#             if s[r] in hashmap:
-#                 hashmap.pop(s[l]) 
-#                 l = l+1
-#             else:
-#                 hashmap[s[r]]=r
-#                 maxcount = max(maxcount, len(hashmap.keys()))               
-#                 r=r+1
-#         return maxcount
-        
-  
-        
-        
-#         p1= 0; p2= 1
-#         sub_len=0
-#         temp_len=0
-                
-#         for p2 in range( len(s)):
-#             print(s[p2])
-#             for j in range(p2-1,0, -1):
-#                 # print(i, j)
-#                 if (s[p2] == s[j]):
-#                     print('matching position :', p2, j)
-#                     p1 = j
-#                     temp_len=p2-j
-#                     if sub_len<temp_len:
-#                         sub_len=temp_len
-#                     break
-#                 temp_len=p2-j
-#                 if sub_len<temp_len:
-#                     sub_len=temp_len
-#         print('string length',sub_len)
-        
-        
-#         print(s)
-#         stack=[]
-#         subStr_len= 0
-#         sub = 0
-#         for i in s: 
-#             stack.append(i)
-#             print(stack)
-            
-#             for j in stack: 
-#                 print(i,j)
-#                 if i ==j:
-#                     stack=[]
-#                     if sub<subStr_len:
-#                         sub=subStr_len
-#                     subStr_len=0
-#                     break
-#                 else:
-#                     stack.append(j)
-#                     subStr_len+=1
-#                 if sub<subStr_len:
-#                         sub=subStr_len   
-            
-#         return sub
-                
