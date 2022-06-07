64f2e325ace3c9186676a76270427c243f7e9559
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-06-07 10:25:08 -0400

    Time: 9505 ms (5.06%), Space: 13.7 MB (74.46%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,9 +1,9 @@
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        if len(s1)>len(s2): return False
         l = 0
         r = len(s1)
         s1Count = collections.Counter(s1)
-        
         while r <= len(s2):
             if s1Count == collections.Counter(s2[l:r]):
                 return True  
@@ -11,3 +11,29 @@ class Solution:
             r += 1
             
         return False
+    
+    
+#         if len(s1)>len(s2): return False
+#         s1Map=collections.Counter(s1)
+#         l=0; r=len(s1)
+#         while r<=len(s2):
+#             if s1Map==collections.Counter(s2[l:r]):
+#                 return True
+#             l+=1
+#             r+=1
+#         return False
+        
+        
+        
+                
+#         l = 0
+#         r = len(s1)
+#         s1Count = collections.Counter(s1)
+#         print(s1Count)
+#         while r <= len(s2):
+#             if s1Count == collections.Counter(s2[l:r]):
+#                 return True  
+#             l += 1
+#             r += 1
+            
+#         return False
