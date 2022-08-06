770640dd0ef8a33600bd7a2b7430148b9d1a9b1e
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-06 12:18:13 -0400

    Time: 675 ms (5.16%), Space: 13.5 MB (61.91%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        res=[]
+        stack=[]
+        candidates.sort()
+
+        def canCombo(cursum, j):
+            print(cursum, j)
+            if cursum == target:
+                print(stack, cursum)
+                if stack not in res:
+                    res.append(stack[::])
+                return 
+            if  j>=len(candidates) or cursum>target:
+                print('caught')
+                return 
+            prev=-1
+            for i in range(j,len(candidates)):
+                if candidates[i]==prev:
+                    continue
+                stack.append(candidates[i])
+                cursum+=candidates[i]
+                canCombo(cursum, i+1)
+                e=stack.pop()
+                cursum-=e
+                prev=candidates[i]
+ 
+
+        canCombo(0,0)
+        return res
+        
