d3efcd14b7aa40136ac301c6c9302f234d4a0f3a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-06 21:40:50 -0400

    Time: 38 ms (57.93%), Space: 13.7 MB (39.58%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,70 @@
 class Solution(object):
     def permute(self, nums):
-        # nums = [1,2,3]
         res=[]
         stack=[]
-        used = [False]*len(nums)
-        def backTrack():
-            if len(stack)==len(nums):
+        used=[False]*len(nums)
+        def dfs (j):
+            if len(stack) == len(nums):
                 res.append(stack[::])
                 return
-
             for i in range(len(nums)):
                 if not used[i]:
-                    stack.append(nums[i])
                     used[i]=True
-                    backTrack()
-                    used[i]=False
+                    stack.append(nums[i])
+                    dfs(i+1)
                     stack.pop()
-        backTrack()
+                    used[i]=False
+        dfs(0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # nums = [1,2,3]
+#         res=[]
+#         stack=[]
+#         used = [False]*len(nums)
+#         def backTrack():
+#             if len(stack)==len(nums):
+#                 res.append(stack[::])
+#                 return
+
+#             for i in range(len(nums)):
+#                 if not used[i]:
+#                     stack.append(nums[i])
+#                     used[i]=True
+#                     backTrack()
+#                     used[i]=False
+#                     stack.pop()
+#         backTrack()
+#         return res
         
