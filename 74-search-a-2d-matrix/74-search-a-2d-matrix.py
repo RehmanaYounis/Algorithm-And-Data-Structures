b947c89721a928114cd75ebc5d0a916197bccaa1
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-07 16:30:58 -0400

    Time: 45 ms (49.89%), Space: 13.8 MB (73.40%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,23 +1,7 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
         rows=len(matrix)
-        cols=len(matrix[0])
-
-#         top, bot = 0, ROWS - 1
-#         while top <= bot:
-#             row = (top + bot) // 2
-#             if target > matrix[row][-1]:
-#                 top = row + 1
-#             elif target < matrix[row][0]:
-#                 bot = row - 1
-#             else:
-#                 break
-
-#         if not (top <= bot):
-#             return False
-#         row = (top + bot) // 2
-        
-        
+        cols=len(matrix[0])    
         start=0
         end=rows-1
         
@@ -48,32 +32,4 @@ class Solution(object):
                 return True
         return False
                 
-        
-        
-        
-        
-#         ROWS, COLS = len(matrix), len(matrix[0])
-
-#         top, bot = 0, ROWS - 1
-#         while top <= bot:
-#             row = (top + bot) // 2
-#             if target > matrix[row][-1]:
-#                 top = row + 1
-#             elif target < matrix[row][0]:
-#                 bot = row - 1
-#             else:
-#                 break
-
-#         if not (top <= bot):
-#             return False
-#         row = (top + bot) // 2
-#         l, r = 0, COLS - 1
-#         while l <= r:
-#             m = (l + r) // 2
-#             if target > matrix[row][m]:
-#                 l = m + 1
-#             elif target < matrix[row][m]:
-#                 r = m - 1
-#             else:
-#                 return True
-#         return False
+  
