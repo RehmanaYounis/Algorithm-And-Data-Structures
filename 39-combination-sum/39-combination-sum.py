6a91d36eb3b5ccbcb1b4dd711c3777b4c0346d67
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-06 21:16:42 -0400

    Time: 131 ms (53.14%), Space: 14.2 MB (36.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,20 +2,73 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
         stack=[]
-        def combSum(curSum, j):
-            if curSum>target:
-                return
-            if curSum== target:
+        def dfs(curSum, j):
+            if curSum==target:
                 res.append(stack[::])
                 return
-            for i in range(j, len(candidates)):
-                    stack.append(candidates[i])
-                    curSum+=candidates[i]
-                    combSum(curSum, i)
-                    temp=stack.pop()
-                    curSum-=temp
-        combSum(0,0)
+            if curSum>target:
+                return
+            for i in range(j,len(candidates)):
+                curSum+=candidates[i]
+                stack.append(candidates[i])
+                dfs(curSum, i)
+                temp=stack.pop()
+                curSum-=temp
+        dfs(0,0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res=[]
+#         stack=[]
+#         def combSum(curSum, j):
+#             if curSum>target:
+#                 return
+#             if curSum== target:
+#                 res.append(stack[::])
+#                 return
+#             for i in range(j, len(candidates)):
+#                     stack.append(candidates[i])
+#                     curSum+=candidates[i]
+#                     combSum(curSum, i)
+#                     temp=stack.pop()
+#                     curSum-=temp
+#         combSum(0,0)
+#         return res
         
         
         
