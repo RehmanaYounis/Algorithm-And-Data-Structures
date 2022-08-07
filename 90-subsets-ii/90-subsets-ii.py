4948eb70750415604320a4c67bc3bec63dce0755
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-06 21:09:29 -0400

    Time: 53 ms (16.32%), Space: 13.6 MB (73.77%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -3,16 +3,66 @@ class Solution(object):
         res=[]
         stack=[]
         nums.sort()
-        def backTrack(i):
-            if i ==len(nums):
+        def dfs(i):
+            if i>=len(nums):
                 res.append(stack[::])
-                return
+                return 
             stack.append(nums[i])
-            backTrack(i+1)
+            dfs(i+1)
             stack.pop()
             while i+1<len(nums) and nums[i]==nums[i+1]:
                 i+=1
-            backTrack(i+1)
-        backTrack(0)
+            dfs(i+1)
+        dfs(0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res=[]
+#         stack=[]
+#         nums.sort()
+#         def backTrack(i):
+#             if i ==len(nums):
+#                 res.append(stack[::])
+#                 return
+#             stack.append(nums[i])
+#             backTrack(i+1)
+#             stack.pop()
+#             while i+1<len(nums) and nums[i]==nums[i+1]:
+#                 i+=1
+#             backTrack(i+1)
+#         backTrack(0)
+#         return res
         
