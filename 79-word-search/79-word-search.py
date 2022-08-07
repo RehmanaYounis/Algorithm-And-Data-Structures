5cbe43874092a83f65300e9d9cedf18faef88521
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-06 22:21:23 -0400

    Time: 5608 ms (78.57%), Space: 13.6 MB (47.31%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,27 +1,93 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        visited=set()
+class Solution(object):
+    def exist(self, board, word):
         rows=len(board)
         cols=len(board[0])
-        
-        def dfs(row, col, i):
-            if i == len(word):
+        visited=set()
+        def dfs(r, c, j):
+            if j==len(word):
                 return True
-            if  (row < 0 or row >= rows or
-                col < 0 or col >= cols or
-                (row, col) in visited or
-                word[i] != board[row][col]):
-                    return False
-            visited.add((row,col))
-            res=  ( dfs(row+1, col, i+1) or
-                    dfs(row-1, col, i+1) or
-                    dfs(row, col+1, i+1) or
-                    dfs(row, col-1, i+1) )
-            visited.remove((row, col))
+            if (r<0 or r>=rows or c<0 or c>=cols or (r,c) in visited or word[j]!= board[r][c]):
+                return False
+            visited.add((r,c))
+            res=(dfs(r+1, c, j+1) or 
+                 dfs(r-1, c, j+1) or 
+                 dfs(r, c+1, j+1) or 
+                 dfs(r, c-1, j+1)       
+                )
+            visited.remove((r,c))
             return res
         
-        for row in range(rows):
-            for col in range(cols):
-                if dfs(row, col, 0):
+        for r in range(rows):
+            for c in range(cols):
+                if (dfs(r,c,0)):
                     return True
         return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         rows= len(board)
+#         columns=len(board[0])
+       
+#     #DFS Function
+#         def dfs(board, word, i, j):
+#             if len(word)==0: return True
+            
+#             if i<0 or i>=rows or j<0 or j>=columns or board[i][j]!=word[0] or board[i][j]=='*': return False
+            
+#             c= board[i][j]
+#             board[i][j]='*'
+#             word=word[1:]
+#             res = dfs(board, word, i-1, j) or dfs(board, word, i+1, j) or  dfs(board, word, i, j-1) or dfs(board,                       word, i, j+1)
+#             board[i][j] =c
+#             return res
+        
+#         # Matrix Cells Traversal
+#         for r in range(rows):
+#             for c in range(columns):
+#                 if (board[r][c] == word[0]) and dfs(board, word, r,c):
+#                     return True
+#         return False
+                    
+                    
+                    
+                    
+                
