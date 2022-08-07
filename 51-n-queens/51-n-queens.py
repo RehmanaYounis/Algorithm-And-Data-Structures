f2fdbe1fede2b8dd225c61b19fe30b6bc9b8a936
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-07 13:13:35 -0400

    Time: 102 ms (31.87%), Space: 13.7 MB (58.37%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,43 +1,39 @@
-class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        res=[]
+class Solution(object):
+    def solveNQueens(self, n):
         grid= [["."]*n for i in range(n)]
-        
-        def Queen( col):
-            if col == n:
-                copy=["".join(row) for row in grid]
-                # print(copy)
-                res.append(copy)
-                
-                return 
-            
-            for row in range(len(grid)):
-                if isValid(row, col):
-                    grid[row][col]='Q'
-                    Queen(col+1)
-                    grid[row][col]='.'
-        
-        def isValid(x,y):
-            # check no Queen Exist in any column in that row
-            for col in range(len(grid)):
-                if grid[x][col]=='Q':
+        res=[] 
+        def isValid(row, col):
+            for c in range(n):
+                if grid[row][c]=="Q":
                     return False
-            r = x; c=y;
-            # Check for diagonal
+            r=row
+            c=col
             while r>=0 and c>=0:
-                if grid[r][c]=='Q':
+                if grid[r][c]=="Q":
                     return False
                 r-=1
                 c-=1
             
-            r = x; c=y;
-            while r<len(grid) and c>=0:
-                if grid[r][c]=='Q':
+            r=row
+            c=col
+            while r<n and c>=0:
+                if grid[r][c]=="Q":
                     return False
                 r+=1
                 c-=1
-            return True
+            return True            
+                
+        
+        
+        def Queen(col):
+            if col==n:
+                copy=["".join(row) for row in grid]
+                res.append(copy)
+                return
+            for row in range(n):
+                if isValid(row, col):
+                    grid[row][col]="Q"
+                    Queen(col+1)
+                    grid[row][col]="."
         Queen(0)
         return res
-        
-        
