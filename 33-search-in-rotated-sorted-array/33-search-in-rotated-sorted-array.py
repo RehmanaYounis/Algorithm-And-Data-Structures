e7c4baf315017e3f8a7c7627472a89ece806fa98
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-08 12:47:00 -0400

    Time: 38 ms (62.50%), Space: 13.6 MB (65.55%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,30 +1,27 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        if len(nums)==0: return -1
-        l=0 ; r=len(nums)-1
-        mid=l
-        while l<r:
-            mid=(l+r)//2
-            if nums[mid]>nums[r]:
-                l=mid+1
+class Solution(object):
+    def search(self, nums, target):
+        left = 0; right = len(nums)-1
+        #Find Element that divides array into two sorted part
+        mid=left
+        while left<right:
+            mid = (left+right)//2
+            if nums[mid]>nums[right]:
+                left = mid+1
             else:
-                r=mid
-        mid=l
-        l=0 ; r=len(nums)-1
-        
-        if target >= nums[mid] and target <= nums[r]:
-            l=mid
+                right=mid
+        mid=left
+        left = 0; right = len(nums)-1
+        if target>=nums[mid] and target<=nums[right]:
+            left=mid
         else:
-            r=mid
+            right = mid
         
-        while l<=r:
-            mid =(l+r)//2
-            if target==nums[mid]:
+        while left<=right:
+            mid=(left+right)//2
+            if target == nums[mid]:
                 return mid
-            if target>=nums[mid]:
-                l=mid+1
+            elif target>nums[mid]:
+                left=mid+1
             else:
-                r=mid-1
+                right=mid-1
         return -1
-        
-                
