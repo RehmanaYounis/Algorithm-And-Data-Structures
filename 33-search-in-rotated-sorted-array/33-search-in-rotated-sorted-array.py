35a640a788a05099514ea9994eff90585eb2247d
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-08 21:59:08 -0400

    Time: 43 ms (47.30%), Space: 13.5 MB (97.73%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,27 +1,27 @@
 class Solution(object):
     def search(self, nums, target):
-        left = 0; right = len(nums)-1
-        #Find Element that divides array into two sorted part
-        mid=left
-        while left<right:
-            mid = (left+right)//2
-            if nums[mid]>nums[right]:
-                left = mid+1
+        l=0
+        r=len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
             else:
-                right=mid
-        mid=left
-        left = 0; right = len(nums)-1
-        if target>=nums[mid] and target<=nums[right]:
-            left=mid
+                r=mid
+        mid=l
+        l=0
+        r=len(nums)-1
+        if target >= nums[mid] and target <= nums[r]:
+            l=mid
         else:
-            right = mid
-        
-        while left<=right:
-            mid=(left+right)//2
-            if target == nums[mid]:
+            r=mid
+        while l<=r:
+            mid =(l+r)//2
+            if target==nums[mid]:
                 return mid
             elif target>nums[mid]:
-                left=mid+1
+                l=mid+1
             else:
-                right=mid-1
+                r=mid -1
         return -1
+        
