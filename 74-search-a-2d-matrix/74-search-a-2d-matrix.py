f52511ee4f7ea4eefa30a61fbd7318bbc6130d39
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-08 20:18:08 -0400

    Time: 58 ms (17.78%), Space: 13.8 MB (48.42%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,32 +1,28 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #Find the row 
+class Solution(object):
+    def searchMatrix(self, matrix, target):
         ROWS=len(matrix)
-        COLS= len(matrix[0])
-        firstRow=0
-        lastRow=ROWS-1
-        
-        while firstRow <= lastRow:
-            row=(firstRow+lastRow)//2
-            if target<matrix[row][0]:
-                lastRow = row -1
-            elif target>matrix[row][-1]:
-                firstRow=row+1
+        COLS=len(matrix[0])
+        top=0
+        bottom=ROWS -1
+        while top<=bottom:
+            row=(top+bottom)//2
+            if target>matrix[row][-1]:
+                top=row+1
+            elif target<matrix[row][0]:
+                bottom=row-1
             else:
                 break
+            print(top, bottom, row)
         
-        startCol=0
-        lastCol = COLS -1
-        print(row)
-        while startCol<=lastCol:
-            mid = (startCol+lastCol)//2
-            if target < matrix[row][mid]:
-                lastCol = mid -1
-            elif target>matrix[row][mid]:
-                startCol = mid+1
-            else:
+        l=0 ; r =COLS-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==matrix[row][mid]:
                 return True
+            elif target>matrix[row][mid]:
+                l=mid+1
+            else:
+                r=mid-1
         return False
-            
-            
-        
+
+  
