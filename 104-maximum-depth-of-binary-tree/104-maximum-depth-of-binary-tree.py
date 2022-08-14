77fca01ab0d08a534b0015a066ceaa0bb8f5b34b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-14 16:38:30 -0400

    Time: 43 ms (93.63%), Space: 16.3 MB (55.36%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -1,137 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-        def maxDepth(self, root):
-            if not root: return 0
-            que = deque()
-            que.append(root)
-            depth=0
-            while que:
-                for i in range(len(que)):
-                    node=que.popleft()
-                    if node.left:
-                        que.append(node.left)
-                    if node.right:
-                        que.append(node.right)
-                depth +=1
-            return depth
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-#             level=0
-#             if not root:
-#                 return 0
-#             q=deque([root])
-#             while q:   
-#                 for i in range(len(q)):
-#                     cur=q.popleft()
-#                     print(cur.val)
-#                     if cur.left:
-#                         q.append(cur.left)
-#                     if cur.right:
-#                         q.append(cur.right)
-#                 level += 1
-#             return level
-            
-            
-                       
-                        
-            
-# #             if not root:
-# #                 return 0
-# #             # return max(1 + self.maxDepth(root.left), 1+self.maxDepth(root.right))
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        depth=[0]
+        def findDepth(root,cur):
+            if not root:
+                depth[0]=max(cur, depth[0])
+                return
+            findDepth(root.left, cur+1)
+            findDepth(root.right, cur+1)
+        findDepth(root, 0)
+        return depth[0]
 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-#     def maxDepth(self, root):
-#         if not root:
-#             return 0
-        
-#         level = 0
-#         q = deque([root])
-        
-#         while q:
-#             for i in range(len(q)):
-#                 node = q.popleft()
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-#             level+=1
-#         return level
-        
-        
-        
-        
-        
-        
-        
-# #         Sol 1:
-# #         if not root:
-# #             return 0
-        
-# #         return 1+ max(self.maxDepth(root.left),self.maxDepth(root.right) )
-        
+            
