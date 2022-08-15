105b5afa004bcbb3a8f4534f5b7f4b6e8041d215
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-15 16:41:51 -0400

    Time: 230 ms (34.79%), Space: 14.8 MB (11.24%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,80 +6,21 @@
 #         self.right = right
 class Solution(object):
     def isSubtree(self, root, subRoot):
-        if not subRoot: return True
-        if not root: return False
-        
-        if (self.isSame(root, subRoot)):
-            return True
-        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-        
-             
-    def isSame(self, root, subRoot):
-        if not root and not subRoot: return True
-        if not root or not subRoot: return False
-        if root.val == subRoot.val:
-            return(self.isSame(root.left, subRoot.left) and self.isSame(root.right, subRoot.right))
-        return False   
+        def isSame(p,q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return (isSame(p.left, q.left) and isSame(p.right, q.right))
 
-                   
-#         if not subRoot: return True
-#         if not root: return False
-#         # if subRoot and not root: return False
-#         if (self.isSame(root, subRoot)):
-#             return True
         
-#         return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-        
-#     def isSame(self, root, subRoot):
-#         if not root and not subRoot: return True
-#         if not root or not subRoot: return False
-#         if root.val==subRoot.val:
-#             return (self.isSame( root.left, subRoot.left) and self.isSame(root.right, subRoot.right))
-#         return False
-            
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        return isSame(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if not subRoot:
-#             return True
-#         if not root:
-#             return False
-#         if self.sameTree(root, subRoot):
-#             return True
-        
-#         return (self.isSubtree(root.left, subRoot) or
-#             self.isSubtree(root.right, subRoot))
-        
-        
-    
-#     def sameTree(self, s, t):
-#         if not s and not t:
-#             return True 
-        
-#         if s and t and s.val == t.val:
-#             return (self.sameTree(s.left, t.left) and
-#             self.sameTree(s.right, t.left))
-#         return False
