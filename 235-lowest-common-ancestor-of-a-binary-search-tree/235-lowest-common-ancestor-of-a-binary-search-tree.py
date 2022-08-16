ebdfb87d643d3d7e3ce59e55767d06084d50c0ab
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-15 20:00:28 -0400

    Time: 78 ms (74.40%), Space: 21.5 MB (51.82%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,66 +7,12 @@
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        if not root:return
-        def LCA (root, p,q):
-            if not root: return
-            if (root.val == p.val or root.val== q.val) or (root.val>p.val and root.val<q.val):
+        if p.val>root.val and q.val>root.val:
+                return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val<root.val and q.val<root.val:
+                return self.lowestCommonAncestor(root.left, p,q)
+        else:
                 return root
-            elif root.val<p.val and root.val<q.val:
-                return LCA(root.right, p,q)
-            elif root.val>p.val and root.val>q.val:
-                return LCA(root.left, p,q)
-            else:
-                return root
-        return LCA(root, p,q)
-
-
-            
-    
-    
-    
-    
-    
-#     cur = root
-#         while cur:
-#             if p.val < cur.val and q.val < cur.val:
-#                 cur=cur.left
-#             elif p.val > cur.val and q.val > cur.val:
-#                 cur=cur.right
-#             else:
-#                 return cur
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         cur = root
-#         while cur:
-#             if p.val < cur.val and q.val < cur.val:
-#                 cur=cur.left
-#             elif p.val> cur.val and q.val >cur.val:
-#                 cur=cur.right
-#             else:
-#                 return cur
