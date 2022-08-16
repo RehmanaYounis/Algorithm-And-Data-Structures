f31d53f05cc0835bd7a62be55d832f84eb744c5a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-15 22:47:29 -0400

    Time: 8 ms (99.91%), Space: 13.7 MB (66.47%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -8,17 +8,47 @@ class Solution(object):
     def levelOrder(self, root):
         if not root:
             return
-        result={}
-        def dfs(root,level):
-            if not root:
-                return
-            if root:
-                if level not in result:
-                    result[level]=[]
-                result[level].append(root.val)
-            dfs(root.left, level+1)
-            dfs(root.right, level+1)
+        level=0
+        q=deque([root])
+        res=[]
+        while q:
+            cur=[]
+            for i in range(len(q)):
+                node = q.popleft()
+                cur.append(node.val)
+                print(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(cur) 
+        return(res)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# Recursive Call       
+#         if not root:
+#             return
+#         result={}
+#         def dfs(root,level):
+#             if not root:
+#                 return
+#             if root:
+#                 if level not in result:
+#                     result[level]=[]
+#                 result[level].append(root.val)
+#             dfs(root.left, level+1)
+#             dfs(root.right, level+1)
             
-        dfs(root,0)
-        res = [x for x in result.values()]
-        return res
+#         dfs(root,0)
+#         res = [x for x in result.values()]
+#         return res
