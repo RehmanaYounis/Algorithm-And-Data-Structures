3043dbd7eb65d869482074d6583b379ae85617fc
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-16 19:59:21 -0400

    Time: 107 ms (58.11%), Space: 26.3 MB (27.12%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,141 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-        def maxPathSum(self, root):
-            maxSum=[float('-inf')]
-            def postTraversal(root):
-                if not root: return 0
-                left= max(postTraversal(root.left), 0)
-                right= max(postTraversal(root.right), 0)
-                maxSum[0]= max(maxSum[0], left+right+root.val)
-                return max(left, right) +  root.val
-            postTraversal(root)
-            return maxSum[0]
+    def maxPathSum(self, root):
+        res=[root.val]
         
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-#             maxSum=[root.val]
-            
-#             def dfs(root):
-#                 if not root:
-#                     return
-#                 left = max(0, dfs(root.left))
-#                 right=max(0, dfs(root.right))
-                
-#                 splitSum=root.val + left +right
-#                 maxSum[0] = max(maxSum[0], splitSum)
-#                 CombineSum = root.val + max(left, right)
-#                 return CombineSum
-#             dfs(root)
-#             return maxSum[0]
-                
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-#     def maxPathSum(self, root):
-#         maxSum = [root.val]
+        def dfs(root):
+            if not root:
+                return 0
+            leftMax=dfs(root.left)
+            rightMax=dfs(root.right)
+            leftMax=max(leftMax, 0)
+            rightMax=max(rightMax, 0)
+            cur = root.val +leftMax +rightMax
+            res[0]=max(res[0], cur)
+            return (root.val+ max(leftMax, rightMax))
         
-#         def dfs(root):
-#             if not root:
-#                 return 0
-#             left = dfs(root.left)
-#             right = dfs(root.right)
-#             left= max(0, left)
-#             right = max(0, right)
-            
-#             splitSum = root.val + left + right
-#             maxSum[0] = max(maxSum[0], splitSum)
-#             CombineSum = root.val+ max(left, right)
-#             return CombineSum
-        
-#         dfs(root)
-#         return maxSum[0]
-        
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-# #         maxSum=[root.val]
-        
-#         def dfs(root):
-#             if not root:
-#                 return 0
-#             leftM = dfs(root.left)
-#             rightM = dfs(root.right)
-#             leftM = max(leftM, 0)
-#             rightM = max(rightM,0)
-
-#             temp= root.val + leftM + rightM
-#             maxSum[0] = max(maxSum[0],temp)
-        
-#             return root.val + max (leftM, rightM)
-        
-#         dfs(root)
-#         return maxSum[0]
-        
+        dfs(root)
+        return res[0]
