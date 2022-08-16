c7821d8b022c062537fc78911297ff8cadb8b892
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-16 12:10:09 -0400

    Time: 34 ms (35.28%), Space: 13.4 MB (77.45%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,17 +6,45 @@
 #         self.right = right
 class Solution(object):
     def rightSideView(self, root):
-        height=[-1]
         res=[]
-        def dfs(root, cur):
-            if not root:
-                return 
-            if cur>height[0]:
-                res.append(root.val)
-                height[0]=cur
-            dfs(root.right,cur+1)
-            dfs(root.left,cur+1)
+        q = deque([root])
+        
+        while q:
+            rightSide=None
+            for i in range(len(q)):
+                node=q.popleft()
+                if node:
+                    rightSide=node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return(res)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         height=[-1]
+#         res=[]
+#         def dfs(root, cur):
+#             if not root:
+#                 return 
+#             if cur>height[0]:
+#                 res.append(root.val)
+#                 height[0]=cur
+#             dfs(root.right,cur+1)
+#             dfs(root.left,cur+1)
            
-        dfs(root, 0)
-        return res
+#         dfs(root, 0)
+#         return res
             
