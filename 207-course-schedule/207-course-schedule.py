606cb97682269f598ec9ddd7f7af5498d5f20146
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-23 17:35:30 -0400

    Time: 99 ms (96.87%), Space: 17.1 MB (40.62%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        cmap={i:[] for i in range((numCourses))}
+        for crs,pre in prerequisites:
+            cmap[crs].append(pre)
+        
+        visited=set()
+        def dfs(cur):
+            if cur in visited:
+                return False
+            if cmap[cur]==[]:
+                return True
+            visited.add(cur)
+            for pre in cmap[cur]:
+                if not dfs(pre): return False
+            visited.remove(cur)
+            cmap[cur]=[]
+            return True
+        for i in range(numCourses):
+            if not dfs(i): return False
+        return True
