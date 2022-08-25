02a7a3c81a0f3c8dadd9837b2b5515df746c2f4e
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-25 16:30:13 -0400

    Time: 677 ms (36.97%), Space: 15.5 MB (85.45%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,20 +1,22 @@
-class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
         nmap=collections.defaultdict(list)
         for u,v,w in times:
             nmap[u].append([v,w])
         
+        visit=set()
         time=0
         minHeap=[[0,k]]
-        visit=set()
         while minHeap:
-            w,node = heapq.heappop(minHeap)
-            if node in visit:
+            w,src = heapq.heappop(minHeap)
+            
+            if src in visit:
                 continue
-            visit.add(node)
             time=w
-            for nei,cw in nmap[node]:
-                if not nei in visit:
-                    heapq.heappush(minHeap, [cw+w, nei])
-        print(len(visit))
+            visit.add(src)
+            for des,dp in nmap[src]:
+                if  not des in visit:
+                    heapq.heappush(minHeap, [w+dp,des])
         return time if len(visit)==n else -1
+    
+ 
