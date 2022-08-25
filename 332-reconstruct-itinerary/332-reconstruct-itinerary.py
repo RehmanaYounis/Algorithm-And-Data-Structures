7f7b6b23ee3b690e9a76bf98d25d1be89935b79c
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-25 17:00:36 -0400

    Time: 69 ms (86.04%), Space: 14 MB (68.02%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,22 +1,27 @@
-class Solution:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+class Solution(object):
+    def findItinerary(self, tickets):
         tickets.sort()
         tmap=collections.defaultdict(list)
-        for src,des in tickets:
-            tmap[src].append(des)
+        for f, t in tickets:
+            tmap[f].append(t)
+        
         res=['JFK']
+       
         def dfs(src):
             if len(res)==len(tickets)+1:
                 return True
             if src not in tmap:
                 return False
-            
-            for index,nei in enumerate(tmap[src]):
+            for i, nei in enumerate(tmap[src]):
                 res.append(nei)
-                tmap[src].pop(index)
-                if dfs(nei): return True
+                tmap[src].remove(nei)
+                if (dfs(nei)):return True
                 res.pop()
-                tmap[src].insert(index,nei)
-            # return False
+                tmap[src].insert(i,nei)
+        
         dfs('JFK')
         return res
+                
+                
+                
+        
