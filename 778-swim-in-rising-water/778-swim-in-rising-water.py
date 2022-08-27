f0c0902f08f54a5d47daf0c83734680747184020
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-27 13:12:18 -0400

    Time: 159 ms (58.01%), Space: 13.9 MB (87.90%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.py
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -1,19 +1,22 @@
-class Solution:
-    def swimInWater(self, grid: List[List[int]]) -> int:
-        n=len(grid)
+class Solution(object):
+    def swimInWater(self, grid):
         minHeap=[[grid[0][0],0,0]]
-        t= grid[0][0]
+        time=0
+        end=len(grid)-1
         directions=[[1,0],[-1,0],[0,1],[0,-1]]
         visit=set()
         visit.add((0,0))
         while minHeap:
-            curT, r,c= heapq.heappop(minHeap)
-            if r==n-1 and c == n-1:
-                return curT
-            for dr , dc in directions:
-                nr, nc = r+dr , c+dc
-                if (nr<0 or nc<0 or nr==n or nc==n or (nr,nc) in visit ):
+            curTime, r,c=heapq.heappop(minHeap)
+            if r==end and c==end:
+                return curTime
+            for dr,dc in directions:
+                nr,nc=r+dr, c+dc
+                if nr<0 or nc<0 or nr>end or nc>end or (nr,nc)in visit:
                     continue
-                heapq.heappush(minHeap, [max(curT,grid[nr][nc]), nr,nc])
+                heapq.heappush(minHeap, ([max(curTime,grid[nr][nc]), nr,nc]))
                 visit.add((nr,nc))
+
+                
+        
         
