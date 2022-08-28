ec031b736d4094bdbff5255899a7de078400eefd
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-08-28 18:51:41 -0400

    Time: 4309 ms (5.01%), Space: 23.2 MB (71.34%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,27 +1,28 @@
-class Solution:
+class Solution(object):
     def Partition(self, left, right, nums):
         p=left
-        pivot=nums[right]
-        
+        pivot =nums[right]
         for i in range(left, right):
-            if nums[i]<= pivot:
+            if nums[i]<=pivot:
                 nums[i],nums[p]=nums[p], nums[i]
                 p+=1
-        nums[right], nums[p]=nums[p],nums[right]
+        nums[p], nums[right]=nums[right], nums[p]
         return p
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    
+    
+    def findKthLargest(self, nums, k):
+        left, right = 0, len(nums)-1
         k=len(nums)-k
-        left=0
-        right=len(nums) -1
-        
         while left<right:
-            par = self.Partition(left, right, nums)
-            if par>k:
-                right=par-1
-            elif par<k:
-                left=par+1
+            p = self.Partition(left, right, nums)
+            if p>k:
+                right=p-1
+            elif p<k:
+                left=p+1
             else:
                 break
         return nums[k]
         
         
+    
+
