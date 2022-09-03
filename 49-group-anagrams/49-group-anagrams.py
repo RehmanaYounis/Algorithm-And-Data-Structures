82a984c741ec08320a2e985774acd1dba6e955c0
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-03 09:03:56 -0400

    Time: 138 ms (49.44%), Space: 17.7 MB (60.44%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,17 +1,10 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        hashmap={}
-        for s in strs:
-            # temp = "".join(sorted(s))
-            temp=tuple(sorted(s))
-            # print(temp)
-            if temp not in hashmap:
-                hashmap[temp]=[s]
-            else:
-                hashmap[temp]=hashmap[temp]+[s]
-                
-        return hashmap.values()
+        aMap=defaultdict(list)
+        for word in strs: 
+            k=''.join(sorted(word))
+            aMap[k].append(word)
+
         
+        return aMap.values()
         
-        
-    
