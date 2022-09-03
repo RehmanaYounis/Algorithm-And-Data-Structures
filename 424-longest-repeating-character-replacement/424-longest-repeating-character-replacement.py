59c9329daa57b14a33d7201ffda476aaed09c862
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-03 17:37:19 -0400

    Time: 155 ms (80.91%), Space: 14.1 MB (19.07%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,14 +1,87 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        hashmap=defaultdict()
-        l=0;
-        maxLen=0; curLen=0;
-        for r in range(len(s)):
+        sMap={}
+        l,r=0,0
+        maxLen=0
+        maxf=0
+        for r in range(len(s)):# r<len(s):
+            sMap[s[r]] = 1+ sMap.get(s[r],0)
+            maxf = max(maxf, sMap[s[r]])
             curLen=r-l+1
-            hashmap[s[r]]=1 + hashmap.get(s[r],0 )
-            while (curLen- max(hashmap.values()) > k):
-                hashmap[s[l]]-=1
+            
+            if curLen - maxf >k:
+                sMap[s[l]]-=1
                 l+=1
-                curLen=r-l+1 
-            maxLen=max(maxLen, curLen)
-        return(maxLen)
+            maxLen=max(maxLen, r-l+1)
+
+        return maxLen
+    
+    
+    
+#         count = {}
+#         res = 0
+
+#         l = 0
+#         maxf = 0
+#         for r in range(len(s)):
+#             count[s[r]] = 1 + count.get(s[r], 0)
+#             maxf = max(maxf, count[s[r]])
+
+#             if (r - l + 1) - maxf > k:
+#                 count[s[l]] -= 1
+#                 l += 1
+
+#             res = max(res, r - l + 1)
+#         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+     # sMap=defaultdict(int)
+     #    l,r=0,0
+     #    maxLen=0
+     #    maxf=0
+     #    while r<len(s):
+     #        sMap[s[r]]+=1
+     #        curLen=r-l+1
+     #        temp = max(maxf, sMap[s[r]])
+     #        while (curLen - temp ) >k:
+     #            sMap[s[l]]-=1
+     #            l+=1
+     #        maxLen=max(maxLen, r-l+1)
+     #        r+=1
+     #    return maxLen
