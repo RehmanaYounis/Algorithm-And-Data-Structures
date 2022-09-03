0555908cfe1a88acb819f2ec5941e8a81f59b589
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-03 17:38:48 -0400

    Time: 307 ms (23.10%), Space: 14 MB (19.07%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -3,86 +3,14 @@ class Solution:
         sMap={}
         l,r=0,0
         maxLen=0
-        maxf=0
         while r<len(s):
-            sMap[s[r]] = 1+ sMap.get(s[r],0)
-            maxf = max(maxf, sMap[s[r]])
-            curLen=r-l+1
-            
+            sMap[s[r]] = 1+ sMap.get(s[r],0)           
+            curLen=r-l+1          
             if curLen - max(sMap.values()) >k:
                 sMap[s[l]]-=1
                 l+=1
             maxLen=max(maxLen, r-l+1)
             r+=1
-
         return maxLen
     
     
-    
-#         count = {}
-#         res = 0
-
-#         l = 0
-#         maxf = 0
-#         for r in range(len(s)):
-#             count[s[r]] = 1 + count.get(s[r], 0)
-#             maxf = max(maxf, count[s[r]])
-
-#             if (r - l + 1) - maxf > k:
-#                 count[s[l]] -= 1
-#                 l += 1
-
-#             res = max(res, r - l + 1)
-#         return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-     # sMap=defaultdict(int)
-     #    l,r=0,0
-     #    maxLen=0
-     #    maxf=0
-     #    while r<len(s):
-     #        sMap[s[r]]+=1
-     #        curLen=r-l+1
-     #        temp = max(maxf, sMap[s[r]])
-     #        while (curLen - temp ) >k:
-     #            sMap[s[l]]-=1
-     #            l+=1
-     #        maxLen=max(maxLen, r-l+1)
-     #        r+=1
-     #    return maxLen
