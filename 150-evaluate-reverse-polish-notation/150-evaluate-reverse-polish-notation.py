f1314d11d2a9e449cd771efa89b11cb67e00c519
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-04 14:51:41 -0400

    Time: 101 ms (58.02%), Space: 14.4 MB (25.75%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,29 +1,28 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack=[]  
-        for r in range(len(tokens)):
-            if tokens[r]=='+':
-                a=int(stack.pop())
-                b=int(stack.pop())
-                res=a+b
-                stack.append(res)
-            elif tokens[r]=='-':
-                a=int(stack.pop())
-                b=int(stack.pop())
-                res=b-a
-                stack.append(res)
-            elif tokens[r]=='*':
-                a=int(stack.pop())
-                b=int(stack.pop())
-                res=a*b
-                stack.append(res)
-            elif tokens[r]=='/':
-                a=int(stack.pop())
-                b=int(stack.pop())
-                res=int(b/a)
-                stack.append(res)
-            else:
-                stack.append(tokens[r])
+        stack=[]
+        for tokken in tokens:
+                cur=tokken
+                if cur == '+':
+                    d1=stack.pop()
+                    d2=stack.pop()
+                    res=d2+d1
+                    stack.append(res)
+                elif cur == '-':
+                    d1=stack.pop()
+                    d2=stack.pop()
+                    res=d2-d1
+                    stack.append(res)
+                elif cur == '*':
+                    d1=stack.pop()
+                    d2=stack.pop()
+                    res=d2*d1
+                    stack.append(res)
+                elif cur == '/':
+                    d1=stack.pop()
+                    d2=stack.pop()
+                    res=int(d2/d1)
+                    stack.append(res)
+                else:
+                    stack.append(int(tokken))
         return stack[0]
-            
-        
