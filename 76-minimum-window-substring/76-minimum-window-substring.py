187162df2602815277748fcf19adb7a3616c4da9
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-04 12:27:02 -0400

    Time: 159 ms (49.26%), Space: 14.2 MB (57.85%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,26 +1,33 @@
 class Solution(object):
     def minWindow(self, s, t):
-        tmap=collections.Counter(t)
-        smap = collections.defaultdict(int)
-        if len(t) > len(s): return ""
-        l, r =0,0
-        output=''
-        minLen = float('inf')
-        need=len(tmap.keys())
+        tmap=Counter(t)
+        smap= {s:0 for s in tmap.keys()}
+        need=len(tmap)
         having = 0
-        for r in range(len(s)):
-            
-            smap[s[r]]+=1;
-            if smap[s[r]] == tmap[s[r]]:
-                having+=1
-            while having == need :
-                if( r-l+1)< minLen:
-                    minLen=r-l+1
-                    output=s[l:r+1]
-                smap[s[l]]-=1
-                # print(s, l)
-                if s[l] in tmap and smap[s[l]]<tmap[s[l]]:
-                    having-=1
+        
+        l=0
+        r=0
+        minLen=float('inf')
+        res=''
+        
+        while r< len(s):
+            cur=s[r]
+            if cur in smap:
+                smap[cur]+=1
+                if smap[cur] == tmap[cur]:
+                    having+=1
+            while having==need:
+                curLen = r-l+1
+                if curLen<minLen:
+                    minLen=curLen
+                    res=s[l:r+1]
+                if s[l] in smap:
+                    smap[s[l]]-=1
+                    if smap[s[l]] < tmap[s[l]]:
+                        having-=1
                 l+=1
-        return (output)
+                
+            r+=1
+        return res
+            
         
