632fb71dae947d6056ff3c53da9c159592745c45
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-04 13:02:51 -0400

    Time: 248 ms (17.05%), Space: 13.8 MB (69.88%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,16 +1,25 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        sMap={}
-        l,r=0,0
+class Solution(object):
+    def characterReplacement(self, s, k):
+        smap=defaultdict(int)
         maxLen=0
+        l=0
+        r=0
         while r<len(s):
-            sMap[s[r]] = 1+ sMap.get(s[r],0)           
-            curLen=r-l+1          
-            if curLen - max(sMap.values()) >k:
-                sMap[s[l]]-=1
-                l+=1
+            smap[s[r]] = 1+ smap.get(s[r],0)    
+            curLen= r-l+1
+            if curLen-max(smap.values())>k:
+                smap[s[l]]-=1
+                l+=1           
             maxLen=max(maxLen, r-l+1)
             r+=1
         return maxLen
-    
+        
+#         sMap[s[r]] = 1+ sMap.get(s[r],0)           
+#             curLen=r-l+1          
+#             if curLen - max(sMap.values()) >k:
+#                 sMap[s[l]]-=1
+#                 l+=1
+#             maxLen=max(maxLen, r-l+1)
+#             r+=1
+#         return maxLen
     
