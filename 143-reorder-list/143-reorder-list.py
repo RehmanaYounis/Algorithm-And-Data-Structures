70e0179a1d74b145085e89fd6f5a7fce195eeded
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-05 19:02:59 -0400

    Time: 138 ms (39.44%), Space: 33.3 MB (34.83%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -1,35 +1,46 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+class Solution(object):
+    def reorderList(self, head):
+        
         slow=head
+        if not slow.next:
+            return head
         fast=head.next
+        
+        dummy=ListNode(0)
+        dummy=slow
         
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-      
-       
-        prev=None
-        cur=slow.next
+            
+        temp=slow.next
+        
         slow.next=None
+        
+        prev=None
+        cur=temp
         while cur:
             temp=cur.next
             cur.next=prev
             prev=cur
             cur=temp
-        first=head
+       
+        first=dummy
         second=prev
-        
-        while second:
+        print(second.val)
+        while first and second:
             temp1=first.next
             temp2=second.next
-            first.next=second
+            first.next = second
             second.next=temp1
-            first=temp1
+            first = temp1
             second=temp2
+        return dummy
         
-   
+        
+        
