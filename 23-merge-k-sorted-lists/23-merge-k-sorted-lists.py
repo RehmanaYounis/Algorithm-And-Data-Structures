8ab7eaf86c299cd53dfb24d23df195623ba2cc27
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-05 17:42:07 -0400

    Time: 5793 ms (5.54%), Space: 19.6 MB (58.12%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,31 +5,53 @@
 #         self.next = next
 class Solution(object):
     def mergeKLists(self, lists):
-        if len(lists)==0: return
-        while len(lists)>1:
-            mergedLists=[]
-
-            for i in range(0, len(lists), 2):
-                l1=lists[i] 
-                l2=lists[i+1] if (i+1) < len(lists) else None
-                mergedLists.append(self.merge(l1, l2))
-            lists=mergedLists
-        return lists[0]
-                
+        if len(lists)==0:
+            return 
+        elif len(lists)==1:
+            return lists[0]
+        res=[]
+        for k in range(len(lists)):
+            res=self.Merge(res, lists[k])
+        return res
     
-    def merge(self, l1, l2):
-        dummy=ListNode(0)
-        cur=dummy
-        while l1 and l2:
-            if l1.val<l2.val:
-                cur.next=l1
-                l1=l1.next
-            else:
-                cur.next=l2
-                l2=l2.next
-            cur=cur.next
-        if l1:
-            cur.next=l1
-        if l2:
-            cur.next=l2
-        return dummy.next
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def Merge(self, head1, head2 ):
+            l1 = head1
+            l2= head2
+            dummy=ListNode(0)
+            temp = dummy
+            
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    dummy.next=l1
+                    l1=l1.next
+                else:
+                    dummy.next=l2
+                    l2=l2.next
+                dummy=dummy.next
+            
+            if l1:
+                dummy.next=l1
+            if l2:
+                dummy.next=l2
+            return temp.next
+        
+        
