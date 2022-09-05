8d99da4133b137bf712a0b3d7802894e1f3cdc26
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-05 12:06:25 -0400

    Time: 94 ms (5.12%), Space: 14.3 MB (17.99%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,27 +1,36 @@
-class Solution(object):
-    def search(self, nums, target):
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         l=0
         r=len(nums)-1
+        mid=(l+r)//2
+        if len(nums)==1:
+            return -1 if not nums[0]==target else 0
         while l<r:
             mid=(l+r)//2
-            if nums[mid]>nums[r]:
-                l=mid+1
-            else:
+            if nums[l]<nums[mid] and nums[mid] >nums[r]:
+                l=mid
+            elif nums[l]>nums[mid]  and nums[mid] <nums[r]:
                 r=mid
-        mid=l
-        l=0
-        r=len(nums)-1
-        if target >= nums[mid] and target <= nums[r]:
-            l=mid
+            else:
+                break
+        
+        mid=mid+1
+        right=len(nums)-1
+        if target >= nums[mid] and target <= nums[right]:
+            left=mid
+            
         else:
-            r=mid
+            left=0
+            right=mid-1
+        print(l,r)
+        l=left
+        r=right
         while l<=r:
-            mid =(l+r)//2
-            if target==nums[mid]:
-                return mid
+            mid=(l+r)//2
+            if target < nums[mid]:
+                r=mid-1
             elif target>nums[mid]:
                 l=mid+1
             else:
-                r=mid -1
+                return mid
         return -1
-        
