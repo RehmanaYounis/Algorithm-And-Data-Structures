3aeabd3806aacec00e7b85e4e0970be3f982a30f
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-05 20:44:49 -0400

    Time: 108 ms (21.03%), Space: 13.1 MB (99.62%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,24 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
         dummy=ListNode(0)
-        ptr=dummy
+        cur=dummy
         carry=0
-        cursum=0
-        while l1 or l2 or carry:
-            d1=l1.val if l1 else 0
-            d2=l2.val if l2 else 0
-            cursum=carry+d1+d2
-            carry=cursum//10
-            cursum=cursum%10
-            print(carry, cursum)
-            if l1 : l1=l1.next 
-            if l2: l2=l2.next 
-            node=ListNode(cursum)
-            ptr.next=node
-            ptr=ptr.next
+        while l1 or l2  or carry:
+            d1= l1.val if l1 else 0
+            d2= l2.val if l2 else 0
+            curSum=d1+d2+carry
+            carry=curSum//10
+            curSum=curSum%10            
+            cur.next= ListNode(curSum)
+            if l1: l1=l1.next
+            if l2: l2=l2.next
+            cur=cur.next
         return dummy.next
+            
+        
