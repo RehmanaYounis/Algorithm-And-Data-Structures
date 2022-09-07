ba46718cfc53d7ee09a97789100129a5bffe88a6
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-07 16:16:14 -0400

    Time: 236 ms (5.07%), Space: 21.3 MB (62.17%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -1,23 +1,40 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxPathSum(self, root):
-        res=[root.val]
-        
-        def dfs(root):
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res=root.val
+        def pathFind(root):
+            nonlocal res
             if not root:
                 return 0
-            leftMax=dfs(root.left)
-            rightMax=dfs(root.right)
-            leftMax=max(leftMax, 0)
-            rightMax=max(rightMax, 0)
-            cur = root.val +leftMax +rightMax
-            res[0]=max(res[0], cur)
-            return (root.val+ max(leftMax, rightMax))
-        
-        dfs(root)
-        return res[0]
+            leftSum=pathFind(root.left)
+            rightSum=pathFind(root.right)
+            leftSum=max(leftSum, 0)
+            rightSum=max(rightSum, 0)
+            res= max(res, root.val+leftSum+rightSum)
+            return root.val+max(leftSum, rightSum)
+        pathFind(root)
+        return res
+    
+#         res = [root.val]
+
+#             # return max path sum without split
+#         def dfs(root):
+#             if not root:
+#                 return 0
+
+#             leftMax = dfs(root.left)
+#             rightMax = dfs(root.right)
+#             leftMax = max(leftMax, 0)
+#             rightMax = max(rightMax, 0)
+
+#             # compute max path sum WITH split
+#             res[0] = max(res[0], root.val + leftMax + rightMax)
+#             return root.val + max(leftMax, rightMax)
+
+#         dfs(root)
+#         return res[0]
