dbdd36a6db870b82f3c69347651e01973cefd250
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-07 19:38:45 -0400

    Time: 9090 ms (8.90%), Space: 17.2 MB (48.71%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,87 +1,41 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
-        self.children = {}
-        self.isWord = False
-        self.refs = 0
+        self.children={}
+        self.eow=False
 
-    def addWord(self, word):
-        cur = self
-        cur.refs += 1
+    def addword(self, word):
+        cur=self
         for c in word:
             if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-            cur.refs += 1
-        cur.isWord = True
-
-    def removeWord(self, word):
-        cur = self
-        cur.refs -= 1
-        for c in word:
-            if c in cur.children:
-                cur = cur.children[c]
-                cur.refs -= 1
-
-
-class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        root = TrieNode()
-        for w in words:
-            root.addWord(w)
-
-        ROWS, COLS = len(board), len(board[0])
-        res, visit = set(), set()
-
-        def dfs(r, c, node, word):
-            if (
-                r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or board[r][c] not in node.children
-                or node.children[board[r][c]].refs < 1
-                or (r, c) in visit
-            ):
+                cur.children[c]=TrieNode()
+            cur=cur.children[c]
+        cur.eow=True
+        
+class Solution(object):
+    def findWords(self, board, words):
+        root=TrieNode()
+        for word in words:
+            root.addword(word)
+        visit=set()
+        ROWS=len(board)
+        COLS=len(board[0])
+        res=set()
+        def dfs(r,c,node,curword):
+            if r<0 or c<0 or r==ROWS or c == COLS or (r,c) in visit or board[r][c] not in node.children:
                 return
-
-            visit.add((r, c))
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.isWord:
-                node.isWord = False
-                res.add(word)
-                root.removeWord(word)
-
-            dfs(r + 1, c, node, word)
-            dfs(r - 1, c, node, word)
-            dfs(r, c + 1, node, word)
-            dfs(r, c - 1, node, word)
-            visit.remove((r, c))
-
+            visit.add((r,c))
+            node=node.children[board[r][c]]
+            curword+=board[r][c]
+            if node.eow:
+                res.add(curword)
+            dfs(r+1,c,node,curword)
+            dfs(r-1,c,node,curword)
+            dfs(r,c+1,node,curword)
+            dfs(r,c-1,node,curword)
+            visit.remove((r,c))
+        
         for r in range(ROWS):
             for c in range(COLS):
-                dfs(r, c, root, "")
-
+                dfs(r,c,root, "")
         return list(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
