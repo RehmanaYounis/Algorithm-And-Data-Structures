4227f3fe1acdb5a602ecf46dbcf568a81c950b8c
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-06 22:39:48 -0400

    Time: 76 ms (8.22%), Space: 14.3 MB (27.32%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,54 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        if not root:
-            return
-        level=0
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:return
         q=deque([root])
         res=[]
         while q:
-            cur=[]
-            for i in range(len(q)):
-                node = q.popleft()
-                cur.append(node.val)
-                print(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            res.append(cur) 
-        return(res)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-# Recursive Call       
-#         if not root:
-#             return
-#         result={}
-#         def dfs(root,level):
-#             if not root:
-#                 return
-#             if root:
-#                 if level not in result:
-#                     result[level]=[]
-#                 result[level].append(root.val)
-#             dfs(root.left, level+1)
-#             dfs(root.right, level+1)
-            
-#         dfs(root,0)
-#         res = [x for x in result.values()]
-#         return res
+            stack=[]
+            for _ in range(len(q)):
+                node=q.popleft()
+                stack.append(node.val)
+                if node.left:q.append(node.left)
+                if node.right:q.append(node.right)
+            res.append(stack)
+        return res
+                    
