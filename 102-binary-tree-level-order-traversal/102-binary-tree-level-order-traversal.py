4daad53ed55907e0bba446eb0ffc6194cb2c7594
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-06 22:47:48 -0400

    Time: 72 ms (12.88%), Space: 15 MB (9.99%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -6,16 +6,35 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:return
-        q=deque([root])
-        res=[]
-        while q:
-            stack=[]
-            for _ in range(len(q)):
-                node=q.popleft()
-                stack.append(node.val)
-                if node.left:q.append(node.left)
-                if node.right:q.append(node.right)
-            res.append(stack)
-        return res
+        res=defaultdict(list)
+        def dfs(root,level):
+            if not root:
+                return
+            res[level].append(root.val)
+            dfs(root.left, level+1)
+            dfs(root.right, level+1)
+        dfs(root, 0)
+        
+        final=[res[i] for i in res.keys()]
+        return final
+                
+        
+        
+        
+        
+        
+        
+        
+#         if not root:return
+#         q=deque([root])
+#         res=[]
+#         while q:
+#             stack=[]
+#             for _ in range(len(q)):
+#                 node=q.popleft()
+#                 stack.append(node.val)
+#                 if node.left:q.append(node.left)
+#                 if node.right:q.append(node.right)
+#             res.append(stack)
+#         return res
                     
