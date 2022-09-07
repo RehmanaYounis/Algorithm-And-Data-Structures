678007396fc040dec503239eaa9c24e6f6b16b70
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-06 23:10:39 -0400

    Time: 66 ms (16.11%), Space: 13.8 MB (70.70%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -1,67 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def rightSideView(self, root):
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        q=deque([root])
         res=[]
-        h=[-1]
-        def dfs(root, level):
-            if not root:
-                return
-            if level>h[0]:
-                h[0]=level
-                res.append(root.val)
-            dfs(root.right,level+1)
-            dfs(root.left, level+1)
-        dfs(root, 0)
+        while q:
+            right=None
+            for i in range(len(q)):
+                node=q.popleft()
+                if node:
+                    right=node
+                    q.append(node.left)
+                    q.append(node.right)
+            if right:
+                res.append(right.val)
         return res
-        
-        
-        
-        
-        
-#         q = deque([root])
-        
-#         while q:
-#             rightSide=None
-#             for i in range(len(q)):
-#                 node=q.popleft()
-#                 # print(node.val)
-#                 if node:
-#                     rightSide=node
-#                     q.append(node.left)
-#                     q.append(node.right)
-#             if rightSide:            
-#                 res.append(rightSide.val)
-#         return(res)
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         height=[-1]
-#         res=[]
-#         def dfs(root, cur):
-#             if not root:
-#                 return 
-#             if cur>height[0]:
-#                 res.append(root.val)
-#                 height[0]=cur
-#             dfs(root.right,cur+1)
-#             dfs(root.left,cur+1)
-           
-#         dfs(root, 0)
-#         return res
-            
+                    
+                
