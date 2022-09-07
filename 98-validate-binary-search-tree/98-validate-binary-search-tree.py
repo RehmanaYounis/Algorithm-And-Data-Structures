d7b61ec18b243f2230cf63d35f7e176a696f193e
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-07 14:14:42 -0400

    Time: 78 ms (36.45%), Space: 16.5 MB (80.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -1,19 +1,76 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def chkBST(root, minv, maxv):
+        def dfs(root, left, right):
             if not root:
                 return True
-            if not (root.val>minv and root.val<maxv):
+            if not ( left<root.val and root.val<right):
                 return False
-            return (chkBST(root.left, minv,root.val ) and chkBST(root.right, root.val,maxv))
-            
-        
-        return chkBST(root, float("-inf"), float("inf"))
-        
+            return (dfs(root.left, left, root.val) and dfs(root.right,root.val, right ))
+
+        return dfs(root, float('-inf'), float('inf'))
+
+
+# def valid(node, left, right):
+#             if not node:
+#                 return True
+#             if not (node.val < right and node.val > left):
+#                 return False
+
+#             return valid(node.left, left, node.val) and valid(
+#                 node.right, node.val, right
+#             )
+
+#         return valid(root, float("-inf"), float("inf"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
