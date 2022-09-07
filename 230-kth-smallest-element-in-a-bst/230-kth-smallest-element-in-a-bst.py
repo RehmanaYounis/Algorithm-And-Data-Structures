7d4e1493d580513dd8211c689bff28b1f33ec3b3
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-07 14:20:04 -0400

    Time: 102 ms (19.55%), Space: 18 MB (88.97%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -1,39 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def kthSmallest(self, root, k):
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         res=[]
-        stack=[]
-        cur=root
-        count=0
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur=cur.left
-            cur=stack.pop()
-            res.append(cur.val)
-            count+=1
-            if count == k:
-                return cur.val
-            cur=cur.right
-        print(res)
-        
-        
-        
-        
-        
-        
-        
-        # Recursion
-        # def dfs(root):
-        #     if not root:
-        #         return
-        #     dfs(root.left)
-        #     res.append(root.val)
-        #     dfs(root.right)
-        # dfs(root)
-        # return(res[k-1])
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+
+        return res[k-1]
