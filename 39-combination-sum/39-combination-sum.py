f8ae06c5a6eae13f837e7113403351f6848b2be4
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-08 15:37:20 -0400

    Time: 402 ms (5.02%), Space: 14.3 MB (8.37%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,110 +2,19 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
         stack=[]
-        def dfs(curSum, j):
+        def dfs(i, curSum):
             if curSum==target:
-                res.append(stack[::])
+                res.append(stack.copy())
+                print(res)
                 return
-            if curSum>target:
+            if i>=len(candidates) or curSum>target:
                 return
-            for i in range(j,len(candidates)):
-                curSum+=candidates[i]
-                stack.append(candidates[i])
-                dfs(curSum, i)
-                temp=stack.pop()
-                curSum-=temp
+            
+            
+            stack.append(candidates[i])
+            dfs(i, curSum+candidates[i])
+            stack.pop()
+            dfs(i+1, curSum)
         dfs(0,0)
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res=[]
-#         stack=[]
-#         def combSum(curSum, j):
-#             if curSum>target:
-#                 return
-#             if curSum== target:
-#                 res.append(stack[::])
-#                 return
-#             for i in range(j, len(candidates)):
-#                     stack.append(candidates[i])
-#                     curSum+=candidates[i]
-#                     combSum(curSum, i)
-#                     temp=stack.pop()
-#                     curSum-=temp
-#         combSum(0,0)
-#         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         output=[]
-#         subset=[]
-#         def backTrack(i, total):
-#             if i >=len(candidates) or total>target:
-#                 return
-#             if total == target:
-#                 print(subset)
-#                 output.append(subset.copy())
-#                 return
-#             subset.append(candidates[i])
-#             backTrack(i, total+candidates[i])
-            
-#             subset.pop()
-#             backTrack(i+1, total)
-            
-#         backTrack(0, 0)
-#         return output
-    
+   
