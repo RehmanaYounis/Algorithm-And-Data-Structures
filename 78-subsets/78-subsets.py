d4afcd7c078c3cfaaad72728830e630f73c4cde0
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-08 14:32:58 -0400

    Time: 65 ms (22.19%), Space: 14.1 MB (35.95%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,118 +1,14 @@
-class Solution(object):
-    def subsets(self, nums):
-        res=[]
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         stack=[]
-        
+        res=[]
         def dfs(i):
-            if i>=len(nums):
-                res.append(stack[::])
-                return 
+            if i ==len(nums):
+                res.append(stack[::-1])
+                return
             stack.append(nums[i])
             dfs(i+1)
             stack.pop()
             dfs(i+1)
         dfs(0)
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        res=[]
-        subset=[]
-        def backTrack(i):
-            if i >= len(nums):
-                res.append(subset.copy())
-                return
-            subset.append(nums[i])
-            backTrack(i+1)
-            subset.pop()
-            backTrack(i+1)
-        backTrack(0)
-        return res
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res=[]
-#         subset=[]
-        
-#         def backtrack(i):
-#             if i >= len(nums):
-#                 print(subset.copy)
-#                 res.append(subset)
-#                 return
-            
-#             subset.append(nums[i])
-#             backtrack(i+1)
-            
-#             subset.pop()
-#             backtrack(i+1)
-#         backtrack(0)
-#         return res
