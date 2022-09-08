1d42d947a0aa7b6bbd842b14cd706b1909fc449a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-08 11:40:57 -0400

    Time: 2237 ms (15.25%), Space: 37.2 MB (7.96%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,28 +1,19 @@
-class Solution(object):
-    def Partition(self, left, right, nums):
-        p=left
-        pivot =nums[right]
-        for i in range(left, right):
-            if nums[i]<=pivot:
-                nums[i],nums[p]=nums[p], nums[i]
-                p+=1
-        nums[p], nums[right]=nums[right], nums[p]
-        return p
-    
-    
-    def findKthLargest(self, nums, k):
-        left, right = 0, len(nums)-1
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
         k=len(nums)-k
-        while left<right:
-            p = self.Partition(left, right, nums)
+        def quickSelect(l,r):
+            pivot=nums[r]
+            p=l
+            for i in range(l,r):
+                if nums[i]<=pivot:
+                    nums[i],nums[p]=nums[p],nums[i]
+                    p+=1
+            nums[p],nums[r]=nums[r],nums[p]
+            
             if p>k:
-                right=p-1
+                return quickSelect(l, p-1)
             elif p<k:
-                left=p+1
+                return quickSelect(p+1, r)
             else:
-                break
-        return nums[k]
-        
-        
-    
-
+                return nums[p]
+        return quickSelect(0, len(nums)-1)
