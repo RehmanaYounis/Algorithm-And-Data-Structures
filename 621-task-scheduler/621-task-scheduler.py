b2244ed29d9e54849cd78a3671a92c0683834ee1
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-08 14:19:06 -0400

    Time: 1046 ms (16.51%), Space: 15.7 MB (26.41%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,21 +1,60 @@
 class Solution(object):
     def leastInterval(self, tasks, n):
-        taskCountMap=Counter(tasks)
+        time =0
         q=deque()
-        time=0
-        minHeap=[-s for s in taskCountMap.values()]
-        heapq.heapify(minHeap)
-        while minHeap or q:
-            # print(minHeap, q)
-            time+=1
-            if minHeap:
-                cur = 1+heapq.heappop(minHeap)
-                if cur !=0:
-                    q.append([cur,time+n])
-            if q and q[0][1]==time:
-                heapq.heappush(minHeap, q.popleft()[0])
-        return time
-            
+        TaskCounter=Counter(tasks)
+        maxHeap = [-s for s in TaskCounter.values() ]
+        heapq.heapify(maxHeap)
         
+        while maxHeap or q:
+            time+=1
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                val = 1+heapq.heappop(maxHeap)
+                if val != 0:
+                    q.append([val, time+n])
+            if q and q[0][1]==time:
+                cur, _ = q.popleft()
+                heapq.heappush(maxHeap, cur)
+        return time
        
+
+# count = Counter(tasks)
+#         maxHeap = [-cnt for cnt in count.values()]
+#         heapq.heapify(maxHeap)
+
+#         time = 0
+#         q = deque()  # pairs of [-cnt, idleTime]
+#         while maxHeap or q:
+#             time += 1
+
+#             if not maxHeap:
+#                 time = q[0][1]
+#             else:
+#                 cnt = 1 + heapq.heappop(maxHeap)
+#                 if cnt:
+#                     q.append([cnt, time + n])
+#             if q and q[0][1] == time:
+#                 heapq.heappush(maxHeap, q.popleft()[0])
+#         return time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
