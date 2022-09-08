7940aceae3cc1a958ba24ab0caa9acdf5f25f9e2
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-09-08 17:44:46 -0400

    Time: 76 ms (84.54%), Space: 14.1 MB (23.53%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,84 +1,25 @@
-class Solution(object):
-    def combinationSum2(self, candidates, target):
-        res=[]
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         stack=[]
-        candidates.sort()
-        def dfs(curSum, j):
-            if curSum==target:
-                res.append(stack[::])
-                return
+        res=[]
+        nums=candidates
+        nums.sort()
+        def dfs(j, curSum):
             if curSum>target:
                 return
+            if curSum==target:
+                res.append(stack[:])
+                return
             prev=-1
-            for i in range(j,len(candidates)):
-                if prev==candidates[i]:
+            for i in range(j, len(nums)):
+                if prev == nums[i]:
                     continue
-                curSum+=candidates[i]
-                stack.append(candidates[i])
-                dfs(curSum, i+1)
-                temp=stack.pop()
-                curSum-=temp
-                prev=candidates[i]
+                stack.append(nums[i])
+                dfs(i+1,curSum+nums[i])
+                stack.pop()
+                prev=nums[i]
         dfs(0,0)
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res=[]
-#         stack=[]
-#         candidates.sort()
-
-#         def canCombo(cursum, j):
-#             if cursum == target:
-#                 print(stack, cursum)
-#                 if stack not in res:
-#                     res.append(stack[::])
-#                 return 
-#             if  j>=len(candidates) or cursum>target:
-#                 return 
-#             prev=-1
-#             for i in range(j,len(candidates)):
-#                 if candidates[i]==prev:
-#                     continue
-#                 stack.append(candidates[i])
-#                 cursum+=candidates[i]
-#                 canCombo(cursum, i+1)
-#                 e=stack.pop()
-#                 cursum-=e
-#                 prev=candidates[i]
- 
-
-#         canCombo(0,0)
-#         return res
-        
+    
+    
+   
