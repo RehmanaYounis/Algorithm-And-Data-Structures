3fae333f049aa09609442088211c9e985adf7494
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-01 13:00:43 -0400

    Time: 217 ms (5.03%), Space: 18.8 MB (23.08%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,18 +1,61 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        if p.val>root.val and q.val>root.val:
-                return self.lowestCommonAncestor(root.right, p, q)
-        elif p.val<root.val and q.val<root.val:
-                return self.lowestCommonAncestor(root.left, p,q)
-        else:
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root):
+        
+         
+            if p.val< root.val and q.val<root.val:
+                return dfs(root.left)
+            
+            if p.val> root.val and q.val>root.val:
+                return dfs(root.right)
+            else:
                 return root
+        return dfs(root)
         
-        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        # if p.val>root.val and q.val>root.val:
+        #         return self.lowestCommonAncestor(root.right, p, q)
+        # elif p.val<root.val and q.val<root.val:
+        #         return self.lowestCommonAncestor(root.left, p,q)
+        # else:
+        #         return root
         
