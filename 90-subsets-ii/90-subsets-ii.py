ac7eeff6949c5ddf38acda80096fc12e42f3b949
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-01 09:05:28 -0400

    Time: 32 ms (75.52%), Space: 13.6 MB (49.30%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,68 +1,18 @@
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def subsetsWithDup(self, nums):
         nums.sort()
         res=[]
-        subset=[]
-        def backTrack(i):
-            if i>=len(nums):
-                if subset not in res:
-                    res.append(subset.copy())
+        stack=[]
+        def dfs(i):
+            if i>= len(nums):
+                res.append(stack[::-1])
                 return
-            
-            subset.append(nums[i])
-            backTrack(i+1)
-            subset.pop()
-            backTrack(i+1)
-        backTrack(0)
+            stack.append(nums[i])
+            dfs(i+1)
+            stack.pop()
+            while i+1< len(nums) and nums[i]==nums[i+1]:
+                i+=1
+            dfs(i+1)
+        dfs(0)
         return res
-            
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         def backTrack(i):
-#             if i>=len(nums):
-#                 if subset not in res:
-#                     res.append(subset.copy())
-#                 return
-            
-#             subset.append(nums[i])
-#             backTrack(i+1)
-#             subset.pop()
-#             backTrack(i+1)
-#         backTrack(0)
-#         return res
-            
