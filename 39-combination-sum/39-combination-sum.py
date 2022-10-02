3bc0f22534a45f9d35c2da6617c8b5dcae08d06c
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-02 12:14:39 -0400

    Time: 96 ms (81.70%), Space: 14 MB (85.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,18 +2,16 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
         stack=[]
-        # target=7
-        def combSum(curSum, j):
-            if curSum>target:
-                return
-            if curSum== target:
+        def calSum(j, curSum):
+            if curSum==target:
                 res.append(stack[::])
                 return
+            elif curSum>target:
+                return
             for i in range(j, len(candidates)):
-                    stack.append(candidates[i])
-                    curSum+=candidates[i]
-                    combSum(curSum, i)
-                    temp=stack.pop()
-                    curSum-=temp
-        combSum(0,0)
+                stack.append(candidates[i])
+                calSum(i, curSum+candidates[i])
+                stack.pop()
+        calSum(0,0)
         return res
+        
