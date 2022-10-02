0df869657225b521353956c1c2ff7274c394757e
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-02 15:46:24 -0400

    Time: 39 ms (79.83%), Space: 13.9 MB (31.45%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        phone={
-               2: 'abc',
-               3: 'def',
-               4:'ghi',
-               5: 'jkl',
-               6: 'mno',
-               7:'pqrs',
-               8:'tuv',
-               9:'wxyz'
-              }
+        res = []
+        if len(digits)==0:
+            return res
+        cMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
         
         stack=[]
-        res=[]
         def dfs(i):
-            if len(digits)==0:
-                return
-            if len(stack)==len(digits):
+            if len(stack) == len(digits):
                 res.append(''.join(stack[::]))
-                return    
-            for char in phone[int(digits[i])]:
+            if i>=len(digits):
+                return 
+            for char in cMap[digits[i]]:
                 stack.append(char)
                 dfs(i+1)
                 stack.pop()
         dfs(0)
         return res
+                
