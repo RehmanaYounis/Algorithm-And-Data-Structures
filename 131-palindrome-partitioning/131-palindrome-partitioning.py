8a5af1bdbfd531ed0a4798203209e34bf6fbab6d
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-02 15:31:25 -0400

    Time: 1507 ms (25.00%), Space: 30.1 MB (66.80%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -5,12 +5,18 @@ class Solution:
         def dfs(j):
             if j>=len(s):
                 res.append(stack[::])
-                return
             for i in range(j, len(s)):
-                curWord = s[j:i+1]
-                if curWord == curWord[::-1]:
-                    stack.append(curWord)
+                if self.isPal(s,j,i):
+                    stack.append(s[j:i+1])
                     dfs(i+1)
                     stack.pop()
         dfs(0)
         return res
+    
+    def isPal(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
