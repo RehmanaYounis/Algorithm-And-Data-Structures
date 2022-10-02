b7fcf80edc4eb4f8b928874ed0a8c51a2242d74b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-02 12:52:02 -0400

    Time: 148 ms (34.87%), Space: 13.7 MB (15.72%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,22 +1,24 @@
 class Solution(object):
     def combinationSum2(self, candidates, target):
+        nums=candidates
+        nums.sort()
         res=[]
         stack=[]
-        candidates.sort()
-        def dfs(curSum,j):
+        def dfs(j, curSum):
             if curSum==target:
-                res.append(stack[::-1])
-                return 
+                res.append(stack[::])
+                return
             if curSum>target:
                 return
             prev=-1
-            for i in range(j,len(candidates)):
-                if prev == candidates[i]:
+            for i in range(j,len(nums)):
+                if prev == nums[i]:
                     continue
-                stack.append(candidates[i])
-                dfs(curSum+candidates[i], i+1)
+                stack.append(nums[i])
+                dfs(i+1, curSum+nums[i])
                 stack.pop()
-                prev=candidates[i]
-        dfs(0,0)
+                prev=nums[i]
+        dfs(0, 0)
         return res
+                
         
