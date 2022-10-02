feeed200831bb57d7cd09b97863d0da6add2fff5
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-02 12:27:44 -0400

    Time: 83 ms (24.36%), Space: 14.1 MB (23.19%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,20 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        stack=[]
         res=[]
+        stack=[]
         visit=[]
-        def dfs(j, stack):
-            
-            if len(stack)>=len(nums):
-                res.append(stack[::-1])
+        def perm():
+            if len(stack) == len(nums):
+                res.append(stack[::])
                 return
             for i in range(len(nums)):
                 if nums[i] not in visit:
                     visit.append(nums[i])
                     stack.append(nums[i])
-                    dfs(i+1,stack)
+                    perm()
                     stack.pop()
                     visit.pop()
-                
-        dfs(0,stack)
+            
+        perm()
         return res
