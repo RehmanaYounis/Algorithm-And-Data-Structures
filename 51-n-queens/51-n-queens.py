d8d14a41d1845d76cddfbff16991b96b1e5f5283
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-03 13:48:38 -0400

    Time: 406 ms (9.04%), Space: 14.5 MB (46.95%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,39 +1,45 @@
-class Solution(object):
-    def solveNQueens(self, n):
-        grid= [["."]*n for i in range(n)]
-        res=[] 
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res=[]
+        rows=n
+        cols=n
+        grid=[]
+        for row in range(rows):
+            stack=[]
+            for col in range(cols):
+                stack.append('.')
+            grid.append(stack)
+        
         def isValid(row, col):
-            for c in range(n):
-                if grid[row][c]=="Q":
+            for c in range(cols):
+                if grid[row][c] =='Q':
                     return False
             r=row
             c=col
             while r>=0 and c>=0:
-                if grid[r][c]=="Q":
+                if grid[r][c]=='Q':
                     return False
                 r-=1
                 c-=1
-            
             r=row
             c=col
-            while r<n and c>=0:
-                if grid[r][c]=="Q":
+            while r<rows  and c>=0:
+                if grid[r][c]=='Q':
                     return False
                 r+=1
                 c-=1
-            return True            
-                
+            return True
         
-        
-        def Queen(col):
+        def dfs(col):
             if col==n:
                 copy=["".join(row) for row in grid]
                 res.append(copy)
-                return
-            for row in range(n):
+                return 
+            
+            for row in range(rows):
                 if isValid(row, col):
-                    grid[row][col]="Q"
-                    Queen(col+1)
-                    grid[row][col]="."
-        Queen(0)
-        return res
+                    grid[row][col]='Q'
+                    dfs(col+1)
+                    grid[row][col]='.'
+        dfs(0)
+        return (res)
