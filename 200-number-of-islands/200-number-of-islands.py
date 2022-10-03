69fe7520d9a1be83da6e5c50cadb5909686dbc7a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-03 16:30:33 -0400

    Time: 344 ms (78.00%), Space: 35.3 MB (5.08%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,28 +1,21 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
         count=0
         rows=len(grid)
         cols=len(grid[0])
-        directions=[[-1,0],[1,0],[0,1],[0,-1]]
         visit=set()
-        if not rows and not cols:
-            return count
-        def dfs(r, c):
-            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]=='0' or (r,c) in visit:
-                return 
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visit or grid[r][c]=='0':
+                return
             visit.add((r,c))
-            for cr,  cc in directions:
-                nr = cr+r
-                nc = cc+c
-                dfs(nr, nc)
-            
-        
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
         for row in range(rows):
             for col in range(cols):
-                if (row, col) not in visit and grid[row][col]=='1':
+                if grid[row][col]=='1' and (row,col) not in visit:
                     count+=1
-                    dfs(row, col)
-                    
-        
+                    dfs(row,col)
         return count
-            
+        
