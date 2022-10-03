a2f64707969b361ade4debc28424589ed675ab97
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-03 18:20:31 -0400

    Time: 251 ms (55.26%), Space: 16.1 MB (40.65%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,32 +1,29 @@
-class Solution(object):
-    def solve(self, board):
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
         rows=len(board)
         cols=len(board[0])
-        visited=set()
-        def dfs(r,c,board):
-            if r<0 or c<0 or r==rows or c == cols or (r,c) in visited or board[r][c] !='O':
+        directions=[[1,0],[-1,0],[0,1],[0,-1]]
+        def dfs(r, c):
+            if r<0 or r>=rows or c <0 or c>=cols or board[r][c]!='O':
                 return
             board[r][c]='R'
-            dfs(r+1,c,board)
-            dfs(r-1,c,board)
-            dfs(r,c+1,board)
-            dfs(r,c-1,board)
-        for c in range(cols):
-            if board[0][c]=='O':
-                dfs(0,c,board)
-            if board[rows-1][c]=='O':
-                dfs(rows-1,c,board)
-        for r in range(rows):
-            if board[r][0]=='O':
-                dfs(r,0,board)
-            if board[r][cols-1]=='O':
-                dfs(r,cols-1,board)
+            for cr, cc   in directions:
+                nr=cr+r
+                nc=cc+c
+                dfs(nr,nc)
                 
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]!='R':
-                    board[r][c]='X'
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c]=='R':
-                    board[r][c]='O'
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O' and row in [0,rows-1] or col in [0,cols-1]:
+                    dfs(row, col)
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'R':
+                    board[row][col] = 'O'            
+                
