c475dcb5110f91a79b65716ac334f276e351fc70
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-03 18:23:30 -0400

    Time: 389 ms (17.05%), Space: 15.9 MB (73.05%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,29 +1,46 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        rows=len(board)
-        cols=len(board[0])
-        directions=[[1,0],[-1,0],[0,1],[0,-1]]
-        def dfs(r, c):
-            if r<0 or r>=rows or c <0 or c>=cols or board[r][c]!='O':
+        ROWS, COLS = len(board), len(board[0])
+
+        def capture(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O":
                 return
-            board[r][c]='R'
-            for cr, cc   in directions:
-                nr=cr+r
-                nc=cc+c
-                dfs(nr,nc)
-                
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == 'O' and row in [0,rows-1] or col in [0,cols-1]:
-                    dfs(row, col)
-        
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == 'O':
-                    board[row][col] = 'X'
-        
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] == 'R':
-                    board[row][col] = 'O'            
-                
+            board[r][c] = "T"
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        # 1. (DFS) Capture unsurrounded regions (O -> T)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                    capture(r, c)
+
+        # 2. Capture surrounded regions (O -> X)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+
+        # 3. Uncapture unsurrounded regions (T -> O)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
