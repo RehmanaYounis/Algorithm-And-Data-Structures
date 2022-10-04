4773504191e586ba59a34e0c79dba5a8300249a8
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-03 20:32:06 -0400

    Time: 58 ms (88.68%), Space: 13.9 MB (91.35%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -2,33 +2,38 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
+        total=0
         q=deque()
-        fresh=0
-        time=0
-        # if grid[0][0]!=2:
-            
-        for r in range (rows):
-            for c in range (cols):
-                if grid[r][c]==2:
-                    q.append([r,c])
-                if grid[r][c]==1:
-                    fresh+=1
-        directions=[[1,0],[-1,0],[0,1],[0,-1]]
-        while fresh>0 and q:
-            
-            for _ in range(len(q)):
-                r,c=q.popleft()
-                for cr,cc in directions:
-                
-                    rm=r+cr
-                    cm=c+cc
-
-                    if (rm<0 or cm<0 or rm==rows or cm==cols or grid[rm][cm]!=1):
-                        continue
-                    grid[rm][cm]=2
-                    q.append([rm,cm])
-                    fresh-=1
-            time+=1
-       
-        return time if fresh==0 else -1
+        directions=[[1,0], [-1, 0], [0,1], [0,-1]]
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]==2:
+                    q.append([row,col])
+                elif grid[row][col]==1:
+                    total+=1
         
+        time=0
+        while q and total>0:
+            for _ in range(len(q)):
+                cr,cc=q.popleft()
+                
+                for row,col in directions:
+                    nr=cr+row
+                    nc=cc+col
+                    
+                    if (nr in range(rows) and nc in range(cols) 
+                        and grid[nr][nc] == 1):
+                        q.append([nr,nc])
+                        grid[nr][nc]=2
+                        total-=1
+            time+=1
+        if total == 0: return time
+        else: return -1
+                        
+                    
+                    
+                
+                
+                
+                
+                
