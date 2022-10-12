ff34d7463dbb3ef2dda8a8667818cb7b7061bab2
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-12 19:02:00 -0400

    Time: 325 ms (20.67%), Space: 14.9 MB (16.51%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS= len(matrix)
+        COLS=len(matrix[0])
+        visit=set()
+        def dfs(r,c,dire):
+            if r>=0 and r<ROWS and c>=0 and c< COLS  and matrix[r][c] !=0:
+        
+                matrix[r][c]='*'
+                if dire == 'U':
+                    dfs(r-1, c, 'U')
+                elif dire == 'D':
+                    dfs(r+1, c, 'D')
+                elif dire =='R':
+                    dfs(r, c+1,'R')
+                else:
+                    dfs(r, c-1,'L')
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c]==0:
+                    dfs(r+1,c, 'D')
+                    dfs(r-1,c, 'U')
+                    dfs(r,c-1, 'L')
+                    dfs(r,c+1, 'R')
+        
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c]=='*':
+                    matrix[r][c]=0
+                    
+        return matrix
+        
