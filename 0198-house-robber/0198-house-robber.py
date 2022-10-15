279a83cc93e24baf805b2b2e2233e1f747d15f68
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-15 10:26:40 -0400

    Time: 34 ms (39.30%), Space: 13.5 MB (15.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,44 +1,18 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        rMap=defaultdict(int)
-        def helper(n):
-            if n==0:
-                return nums[0]
-            if n<0:
+class Solution(object):
+    
+    def rob(self, nums):
+        hashMap={}
+        def dfs(n):
+            if n>=len(nums):
                 return 0
-            if n in rMap:
-                return rMap[n]
-            choice1= nums[n]+helper(n-2)
-            choice2= helper(n-1)
-            rMap[n]=max(choice1, choice2)
-            return max(choice1, choice2)
+            if n in hashMap:
+                return hashMap[n]
+            choice1=nums[n]+dfs(n+2)
+            choice2=dfs(n+1)
+            hashMap[n]= max(choice1, choice2)
+            return hashMap[n]
         
-        return helper(len(nums)-1)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         n=len(nums)
-#         if len(nums)<3:
-#             return max(nums)
-#         elif len(nums)==3:
-#             return max(nums[n-3]+nums[n-1], nums[1])
-       
-#         nums[n-3]=nums[n-3]+nums[n-1]
-#         for i in range(n-4, -1, -1):
-#             nums[i]=max(nums[i]+nums[i+2], nums[i]+nums[i+3])
-#         print(nums)
-#         return max(nums[0], nums[1])
+        maxCost=float('-inf')
+        for i in range(len(nums)):
+            maxCost=max(maxCost, dfs(i))
+        return maxCost
