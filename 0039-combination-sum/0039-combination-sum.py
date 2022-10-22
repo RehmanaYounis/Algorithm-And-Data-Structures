9b9614b2e228dab2e1c27bfdf68c11373ec4d115
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-22 19:23:48 -0400

    Time: 61 ms (96.22%), Space: 14.1 MB (47.14%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -9,13 +9,11 @@ class Solution:
                 return True
             if curSum<0:
                 return False
-            prev=-1
             for i in range(j, len(candidates)):
-                if nums[i]!=prev:
-                    stack.append(nums[i])
-                    if dfs(i, curSum-nums[i]):
-                        res.append(stack[::])
-                    stack.pop()
+                stack.append(nums[i])
+                if dfs(i, curSum-nums[i]):
+                    res.append(stack[::])
+                stack.pop()
         dfs(0,target)
         return res
             
