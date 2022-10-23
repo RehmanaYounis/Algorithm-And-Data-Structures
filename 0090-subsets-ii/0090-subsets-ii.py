4c1c320c97eab570da80c00116a952524cd520bb
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-22 21:18:25 -0400

    Time: 50 ms (33.80%), Space: 13.5 MB (72.50%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        res=[]
+        stack=[]
+        nums.sort()
+        def dfs(i):
+            if i>=len(nums):
+                res.append(stack[::])
+                return
+            stack.append(nums[i])
+            dfs(i+1)
+            
+            stack.pop()
+            while i+1<len(nums) and nums[i] == nums[i+1]:
+                i+=1
+            dfs(i+1)
+        dfs(0)
+        return res
