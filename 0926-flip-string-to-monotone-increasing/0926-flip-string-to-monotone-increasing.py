1d6649f423841b8f8fa8978269597e03cef667fe
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-24 00:10:32 -0400

    Time: 502 ms (52.24%), Space: 15.3 MB (43.71%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,16 +1,15 @@
-class Solution(object):
-    def minFlipsMonoIncr(self, s):
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
         oneCount=0
-        posblFlip=0
+        potentFlip=0
         i=0
-        while s[i] !='1':
+        while s[i] != '1':
             i+=1
-        for ind in range(i, len(s)):
-            if s[ind]=='1':
+        for i in range(i, len(s)):
+            if s[i]=='1':
                 oneCount+=1
-            else:
-                posblFlip+=1
-            if posblFlip>oneCount:
-                posblFlip=oneCount
-        return posblFlip
-        
+            elif s[i]=='0':
+                potentFlip+=1
+            if potentFlip>oneCount:
+                potentFlip=oneCount
+        return potentFlip
