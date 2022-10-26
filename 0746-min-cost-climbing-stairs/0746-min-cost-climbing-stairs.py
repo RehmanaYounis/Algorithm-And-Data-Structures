e6b4659401d46505394fd361b7b6be8fad925b85
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-26 12:56:15 -0400

    Time: 77 ms (56.47%), Space: 16.3 MB (5.99%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
-        hashMap={}
+        dp={}
         def dfs(n):
+            if n in dp:
+                return dp[n]
             if n>=len(cost):
                 return 0
-            if n in hashMap:
-                return hashMap[n]
-            withOneStep=cost[n]+dfs(n+1)
-            withTwoStep=cost[n]+dfs(n+2)
-            hashMap[n]=min(withOneStep, withTwoStep)
-            return hashMap[n]
+            print(n)
+            case1=cost[n]+dfs(n+1)
+            case2=cost[n]+dfs(n+2)
+            dp[n]=min (case1, case2)
+            return dp[n]
         return min(dfs(0), dfs(1))
