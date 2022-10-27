8bafd82546521c1dc8d82656eec77719b72bc940
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-26 20:34:51 -0400

    Time: 1491 ms (73.30%), Space: 56.6 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,16 +4,18 @@ class Solution(object):
         def dfs(amount):
             if amount in dp:
                 return dp[amount]
-            if amount == 0:
+            if amount==0:
                 return 0
             if amount<0:
                 return -1
-            pathMin=float('inf')
+            minCoin=float('inf')
             for i in coins:
                 res=dfs(amount-i)
-                if res != -1:
-                    cur= 1+res
-                    pathMin=min(cur, pathMin)
-            dp[amount]=pathMin
+                if res !=-1:
+                    cur=1+res
+                    minCoin=min(cur,minCoin)
+                    
+            dp[amount]=minCoin
             return dp[amount]
-        return dfs(amount) if dfs(amount) != float('inf') else -1
+        return dfs(amount) if dfs(amount)!= float('inf') else -1
+        
