8eebdfed770995c1f54e5944885035758b06f99e
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-27 22:32:28 -0400

    Time: 2916 ms (5.00%), Space: 45.1 MB (5.06%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestIncreasingPath(self, matrix):
+        rows=len(matrix)
+        cols=len(matrix[0])
+        maxPath=0
+        dp={}
+        def dfs(r,c, visit,prev):
+           
+            nonlocal maxPath
+            if (r,c,prev) in dp:
+                return dp[(r,c,prev)]
+            if r<0 or r>=rows or c<0 or c>=cols or [r,c] in visit or matrix[r][c]<=prev:
+                return 0
+            visit.append([r,c])
+            
+
+            # maxPath=max(maxPath, len(visit))
+            right=dfs(r+1,c,visit,matrix[r][c])
+            left=dfs(r-1,c,visit,matrix[r][c])
+            down=dfs(r,c+1,visit,matrix[r][c])
+            bottom=dfs(r,c-1,visit,matrix[r][c])
+            visit.pop()
+            dp[(r,c,prev)]=1+max(right,left,down,bottom)
+            return dp[(r,c,prev)]
+        for i in range(rows):
+            for j in range(cols):
+                res=dfs(i,j,[],float('-inf'))
+                maxPath=max(res, maxPath)
+        return maxPath
