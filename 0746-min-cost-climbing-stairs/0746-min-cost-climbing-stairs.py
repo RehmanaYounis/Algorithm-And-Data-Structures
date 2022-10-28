107c03c6d605f92bf1e2f054b77ce7de2a8d79d4
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-27 23:11:11 -0400

    Time: 99 ms (19.65%), Space: 16 MB (5.68%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,11 +4,55 @@ class Solution(object):
         def dfs(n):
             if n in dp:
                 return dp[n]
-            if n>=len(cost):
+            if n >=len(cost):
                 return 0
-            print(n)
-            case1=cost[n]+dfs(n+1)
+            case1= cost[n]+dfs(n+1)
             case2=cost[n]+dfs(n+2)
-            dp[n]=min (case1, case2)
+            dp[n]=min(case1, case2)
             return dp[n]
+        
+        
         return min(dfs(0), dfs(1))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp={}
+#         def dfs(n):
+#             if n in dp:
+#                 return dp[n]
+#             if n>=len(cost):
+#                 return 0
+#             print(n)
+#             case1=cost[n]+dfs(n+1)
+#             case2=cost[n]+dfs(n+2)
+#             dp[n]=min (case1, case2)
+#             return dp[n]
+#         return min(dfs(0), dfs(1))
