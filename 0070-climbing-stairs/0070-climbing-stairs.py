bda30c56116f7994bb13a1ac9fb50d7715f72a49
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-27 22:47:27 -0400

    Time: 48 ms (67.18%), Space: 13.9 MB (57.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -10,6 +10,48 @@ class Solution:
                 return 0
             case1=dfs(n-1)
             case2=dfs(n-2)
-            dp[n]= case1+case2
+            dp[n] =case1+case2
             return dp[n]
         return dfs(n)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp={}
+#         def dfs(n):
+#             if n in dp:
+#                 return dp[n]
+#             if n==0:
+#                 return 1
+#             if n<0:
+#                 return 0
+#             case1=dfs(n-1)
+#             case2=dfs(n-2)
+#             dp[n]= case1+case2
+#             return dp[n]
+#         return dfs(n)
