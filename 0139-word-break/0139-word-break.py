f8e96565cc53722d22a8e2d3866b8dfca7acc3c3
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-30 11:46:34 -0400

    Time: 90 ms (23.61%), Space: 14.1 MB (25.05%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,19 +2,68 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp={}
         def dfs(i):
-            if i in dp:
+            if i in dp: 
                 return dp[i]
             if i>=len(s):
                 return True
-            res=False
-            print(i)
             for j in range(i, len(s)):
                 word=s[i:j+1]
                 if word in wordDict:
-                    res=dfs(j+1)
-                    if res:
-                        break
-            dp[i]=res
+                    if dfs(j+1):
+                        dp[i]=True
+                        return dp[i]
+            dp[i]=False
             return dp[i]
         return dfs(0)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp={}
+#         def dfs(i):
+#             if i in dp:
+#                 return dp[i]
+#             if i>=len(s):
+#                 return True
+#             res=False
+#             print(i)
+#             for j in range(i, len(s)):
+#                 word=s[i:j+1]
+#                 if word in wordDict:
+#                     res=dfs(j+1)
+#                     if res:
+#                         break
+#             dp[i]=res
+#             return dp[i]
+#         return dfs(0)
                     
