699f972783888f8ae3ccda4e42ce5cb8a3b82243
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-30 14:29:08 -0400

    Time: 2510 ms (51.27%), Space: 14.6 MB (75.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -26,78 +26,10 @@ class Solution:
             if dfs(target,i):       
                 return True
         return False
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if sum(nums)%2 !=0:
-#             return False
-#         target=sum(nums)//2
-#         dp={}
-#         nums.sort()
-#         def dfs(target, j):
-#             if (target, j) in dp:
-#                 return dp[(target, j)]
-#             if target ==0:
-#                 return True
-#             if target <0:
-#                 return False
-#             dp[(target,j)]=False
-#             for i in range(j, len(nums)):
-#                 if dfs(target-nums[j],i+1):
-#                     dp[(target,j)]=True
-#                     return dp[(target,j)]
-            
-#             return dp[(target,j)]
-#             return False
-#         return dfs(target,0)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         target=sum(nums)%2
-#         if target==0:
+    
+    
+    
+    
 #             target=sum(nums)//2
 #         else:
 #             return False
