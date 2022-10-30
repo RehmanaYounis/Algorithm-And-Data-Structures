7d21db79455a6854417ed06438cc3491a2b2e52a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-30 14:21:14 -0400

    Time: 3926 ms (30.95%), Space: 14.5 MB (75.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -26,3 +26,99 @@ class Solution:
             if dfs(target,i):       
                 return True
         return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if sum(nums)%2 !=0:
+#             return False
+#         target=sum(nums)//2
+#         dp={}
+#         nums.sort()
+#         def dfs(target, j):
+#             if (target, j) in dp:
+#                 return dp[(target, j)]
+#             if target ==0:
+#                 return True
+#             if target <0:
+#                 return False
+#             dp[(target,j)]=False
+#             for i in range(j, len(nums)):
+#                 if dfs(target-nums[j],i+1):
+#                     dp[(target,j)]=True
+#                     return dp[(target,j)]
+            
+#             return dp[(target,j)]
+#             return False
+#         return dfs(target,0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         target=sum(nums)%2
+#         if target==0:
+#             target=sum(nums)//2
+#         else:
+#             return False
+        
+#         nums.sort()
+#         def dfs(curSum,j):
+#             if curSum in dp:
+#                 return dp[curSum]
+#             if curSum==0:
+#                 return True
+#             if curSum<0:
+#                 return False
+#             for i in range(j, len(nums)):
+#                 if dfs(curSum-nums[i], i+1):
+#                     dp[curSum]=True
+#                     return True
+#             dp[curSum]=False
+#             return dp[curSum]
+        
+#         for i in range(len(nums)):
+#             dp={}
+#             if dfs(target,i):       
+#                 return True
+#         return False
