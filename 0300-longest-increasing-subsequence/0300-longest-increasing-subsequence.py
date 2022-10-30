c10928a1be5829dcedec671ca914b486c5ffc468
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-30 13:15:52 -0400

    Time: 3512 ms (58.46%), Space: 115.1 MB (5.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,79 +1,54 @@
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        dp={}
-        maxLen=1
+class Solution(object):
+    def lengthOfLIS(self, nums):
         
+#         def dfs(i):
+#             if i>=len(nums):
+#                 return 0
+            
+#             res=0
+#             for j in range(i, len(nums)):
+#                 if j+1< len(nums) and nums[j]<nums[j+1]:
+#                     cur=1+dfs(j+1)
+#                     res=max(res,cur)
+#                     print(j, res)
+#             return res
+#         dfs(0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        maxLen=1
+        dp={}
         def dfs(i):
             if i in dp:
                 return dp[i]
-            if i>=len(nums):
-                return 0
-            maximum=1
+            maxLen=0
+            curVal=nums[i]
             for j in range(i+1, len(nums)):
-                if nums[i]<nums[j]:
-                    # print(nums[i], nums[j])
-                    res=1+dfs(j)
-                    maximum=max(maximum, res)
-            dp[i]=maximum
+                if curVal<nums[j]:
+                    maxLen=max(maxLen, dfs(j))
+            dp[i]=maxLen+1
             return dp[i]
-            
         
-        
-        
-        
-        for n in range(len(nums)):
-            res=dfs(n)
-            maxLen=max(maxLen,res)
+        for i in range(len(nums)):
+            maxLen=max(maxLen,dfs(i))
         return maxLen
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         maxLen=1
-#         dp={}
-#         def dfs(i):
-#             if i in dp:
-#                 return dp[i]
-#             maxLen=0
-#             curVal=nums[i]
-#             for j in range(i+1, len(nums)):
-#                 if curVal<nums[j]:
-#                     maxLen=max(maxLen, dfs(j))
-#             dp[i]=maxLen+1
-#             return dp[i]
-        
-#         for i in range(len(nums)):
-#             maxLen=max(maxLen,dfs(i))
-#         return maxLen
     
-        
