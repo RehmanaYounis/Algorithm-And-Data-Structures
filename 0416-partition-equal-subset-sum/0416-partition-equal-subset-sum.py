308052b7822f3961b7fc74c926aac573d654d562
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-30 14:32:14 -0400

    Time: 4648 ms (25.35%), Space: 14.4 MB (75.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,35 +1,139 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        target=sum(nums)%2
-        if target==0:
-            target=sum(nums)//2
-        else:
+        if sum(nums)%2 !=0:
             return False
-        
+        target=sum(nums)//2
+        # dp={}
         nums.sort()
-        def dfs(curSum,j):
-            if curSum in dp:
-                return dp[curSum]
-            if curSum==0:
+        def dfs(target, j):
+            if target in dp:
+                return dp[target]
+            if target ==0:
                 return True
-            if curSum<0:
+            if target <0:
                 return False
+            
             for i in range(j, len(nums)):
-                if dfs(curSum-nums[i], i+1):
-                    dp[curSum]=True
-                    return True
-            dp[curSum]=False
-            return dp[curSum]
+                if dfs(target-nums[i],i+1):
+                    dp[target]=True
+                    return dp[target]
+            dp[target]=False
+            return dp[target]
+        
         
         for i in range(len(nums)):
             dp={}
-            if dfs(target,i):       
+            if dfs(target,i):
                 return True
         return False
     
     
     
     
+   
+        
+        
+            
+#             for i in range(j, len(nums)):
+#                 if dfs(curSum-nums[i], i+1):
+#                     dp[curSum]=True
+#                     return True
+#             dp[curSum]=False
+#             return dp[curSum]
+        
+#         for i in range(len(nums)):
+#             dp={}
+#             if dfs(target,i):       
+#                 return True
+#         return False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        # return dfs(target,0)
+        
+        
+#         target=sum(nums)%2
+#         if target==0:
+#             target=sum(nums)//2
+#         else:
+#             return False
+        
+#         nums.sort()
+#         def dfs(curSum,j):
+#             if curSum in dp:
+#                 return dp[curSum]
+#             if curSum==0:
+#                 return True
+#             if curSum<0:
+#                 return False
+#             for i in range(j, len(nums)):
+#                 if dfs(curSum-nums[i], i+1):
+#                     dp[curSum]=True
+#                     return True
+#             dp[curSum]=False
+#             return dp[curSum]
+        
+#         for i in range(len(nums)):
+#             dp={}
+#             if dfs(target,i):       
+#                 return True
+#         return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         target=sum(nums)%2
+#         if target==0:
 #             target=sum(nums)//2
 #         else:
 #             return False
