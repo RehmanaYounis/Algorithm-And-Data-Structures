c9659d3471bada1edb1fd84daab3556bd1902748
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-31 12:44:22 -0400

    Time: 9067 ms (5.04%), Space: 114.6 MB (6.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,23 +1,75 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
+        dp={}
         def dfs(i):
             if i in dp:
                 return dp[i]
             if i >=len(nums):
                 return 0
-            maxLen=1
-            for j in range(i+1,len(nums)):
+            curMax=1
+            for j in range(i+1, len(nums)):
                 if nums[i]<nums[j]:
                     cur=1+dfs(j)
-                    maxLen=max(maxLen, cur)
-            dp[i]=maxLen
+                    curMax=max(curMax, cur)
+                    # return curMax
+            dp[i]=curMax
             return dp[i]
-        
         maxLen=1
-        dp={}
-        for i in range(len(nums)):  
+        for i in range(len(nums)):
+           
             maxLen=max(maxLen, dfs(i))
         return maxLen
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def dfs(i):
+#             if i in dp:
+#                 return dp[i]
+#             if i >=len(nums):
+#                 return 0
+#             maxLen=1
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]<nums[j]:
+#                     cur=1+dfs(j)
+#                     maxLen=max(maxLen, cur)
+#             dp[i]=maxLen
+#             return dp[i]
+        
+#         maxLen=1
+#         dp={}
+#         for i in range(len(nums)):  
+#             maxLen=max(maxLen, dfs(i))
+#         return maxLen
        
         
         
