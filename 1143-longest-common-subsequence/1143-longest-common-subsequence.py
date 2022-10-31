b4532d1ae363a9e0d0c7e0f4139c3ef680a97b77
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-31 13:57:55 -0400

    Time: 2801 ms (6.12%), Space: 139.6 MB (16.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,14 +6,14 @@ class Solution:
                 return dp[(i,j)]
             if i>= len(text1) or j>= len(text2):
                 return 0
-            
+            dp[(i,j)]=0
             if text1[i]==text2[j]:
                 dp[(i,j)]=1+ dfs(i+1, j+1)
-                return dp[(i,j)]
+                
             else:
                 case1 =  dfs(i+1, j)
                 case2 = dfs(i,j+1)
                 dp[(i,j)]= max(case1, case2)
-                return dp[(i,j)]
+            return dp[(i,j)]
 
         return dfs(0,0)
