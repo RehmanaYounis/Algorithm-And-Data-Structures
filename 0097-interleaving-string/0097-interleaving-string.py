cfd639f0fc4ed4d7fdfa17d3a502572e0bf034d7
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-10-31 16:33:41 -0400

    Time: 53 ms (85.97%), Space: 15.7 MB (18.04%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        dp={}
-        if len(s1)+len(s2)!=len(s3):
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s3) != len(s1)+len(s2):
             return False
-        def dfs(i1, i2):
-            if (i1, i2) in dp:
-                return dp[(i1,i2)]
-            if i1==len(s1) and i2==len(s2):
+        dp={}
+        def dfs(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if i>=len(s1) and j >=len(s2):
                 return True
             case1=False
             case2=False
-            if i1<len(s1) and s1[i1]==s3[i1+i2]:
-                case1=dfs(i1+1, i2)
-            if i2<len(s2) and s2[i2]==s3[i1+i2]:
-                case2=dfs(i1, i2+1)
-            if case1 or case2: dp[(i1, i2)]=True
-            else: dp[(i1, i2)]=False
-            return dp[(i1, i2)]
+            if i<len(s1) and s1[i]==s3[i+j]:
+                case1= dfs(i+1, j)
+            if j<len(s2) and s2[j]==s3[i+j]:
+                case2= dfs(i, j+1)
+            dp[(i,j)]=case1 or case2
+            
+            return dp[(i,j)]
         return dfs(0,0)
-        
