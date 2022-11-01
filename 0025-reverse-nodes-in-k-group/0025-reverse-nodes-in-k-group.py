3b7b96ddc3de960a2542c977977e8454b3dec586
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-11-01 12:32:47 -0400

    Time: 69 ms (58.41%), Space: 15 MB (90.55%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,37 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy=ListNode(0,head)
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        dummy=ListNode(0, head)
         grpPrev=dummy
-
         while True:
             kth=self.getKthElements(grpPrev, k)
             if not kth:
-                break
+                break 
             grpNxt=kth.next
-            prev = grpNxt
-            cur = grpPrev.next
+            cur=grpPrev.next
+            prev=grpNxt
             while cur != grpNxt:
-                tmp=cur.next
+                
+                temp=cur.next
                 cur.next=prev
                 prev=cur
-                cur=tmp
-            
-            tmp=grpPrev.next
+                cur=temp
+            temp=grpPrev.next
             grpPrev.next=kth
-            grpPrev=tmp
+            grpPrev=temp
         return dummy.next
-        
-   
-        
-        
-    def getKthElements(self, cur, k):
+    def getKthElements(self, grpPrev, k):
+        cur=grpPrev
         while cur and k>0:
             cur=cur.next
             k-=1
         return cur
-        
