ca6cb6227a7f5fed9801d0bee3b66a15b23346e7
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-11-01 18:34:11 -0400

    Time: 155 ms (70.67%), Space: 24 MB (37.92%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,33 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reorderList(self, head):
-        list1=head
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        dummy=ListNode(0,head)
         slow=head
-        fast=head.next
+        fast=head
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-        list2=slow.next
+        
+        ptr2=slow.next
         slow.next=None
-        
         prev=None
-        while list2:
-            nxt=list2.next
-            list2.next=prev
-            prev=list2
-            list2=nxt
-        list2=prev
+        cur=ptr2
+        while cur:
+            temp=cur.next
+            cur.next=prev
+            prev=cur
+            cur=temp
+        ptr1=head
+        ptr2 = prev
         
-        while list1 and list2:
-            temp1=list1.next
-            temp2=list2.next
-            list1.next=list2
-            list2.next=temp1
-            list1=temp1
-            list2=temp2
-        return head
-            
+        # print(ptr1)
+        # print(ptr2)
+        while ptr1 and ptr2:
+            temp1=ptr1.next
+            temp2=ptr2.next
+            ptr1.next=ptr2
+            ptr2.next=temp1
+            ptr1=temp1
+            ptr2=temp2
+        return dummy.next
