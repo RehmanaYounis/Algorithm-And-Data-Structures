a1f4a84d3d49cb5629f7c7bedea2f9524d740e74
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2022-11-01 18:15:47 -0400

    Time: 78 ms (7.13%), Space: 13.9 MB (71.12%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,18 +1,58 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        dummy=ListNode(0,head)
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy=ListNode(0, head)
         slow=dummy
         fast=head
-        while n:
+        
+        while n>0:
             fast=fast.next
             n-=1
+        
         while fast:
-            slow=slow.next
             fast=fast.next
-        slow.next=slow.next.next
+            slow=slow.next
+            
+        slow.next=slow.next.next 
         return dummy.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dummy=ListNode(0, head)
+#         slow=dummy
+#         fast=head
+#         k=n
+#         while k>0:
+#             fast=fast.next
+#             k-=1
+#         while fast:
+#             slow=slow.next
+#             fast=fast.next
+#         slow.next=slow.next.next
+#         return dummy.next
