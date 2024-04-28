239990c421182ff2d0c489e386b21d05d17c6198
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-04-28 17:29:40 -0400

    Time: 34 ms (57.27%), Space: 13.7 MB (10.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t): return False
+        sMap, tMap ={}, {}
+        for i in range(len(s)):
+            sMap[s[i]] =1 + sMap.get(s[i],0)
+            tMap[t[i]] =1 + tMap.get(t[i],0)
+        for key in sMap:
+            print(key, sMap[key],sMap[key])
+            if sMap[key] != tMap.get(key, 0):       
+                return False
+            
+        return True
+            
+#             sMap[s[i]] =1 + sMap.get(s[i],0)
+#         print (sMap)
+    
+    
+    
+    
+        # smap=Counter(s)
+        # tmap=Counter(t)
+        # if smap==tmap:
+        #     return True
+        # else:
+        #     return False
