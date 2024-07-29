b0472a6d44287a0d4307016f5b751f9c6f2dd255
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-07-30 02:24:37 +0300

    Time: 25 ms (63.20%), Space: 16 MB (93.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,18 +1,42 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        def isValid(root, minVal, maxVal):
-            if not root:
-                return True
-            if not (root.val > minVal and root.val < maxVal):
-                    return False
-            return (isValid(root.left,minVal, root.val ) and isValid(root.right, root.val, maxVal))
+class Solution(object):
+    def isValidBST(self, root):
+        if not root: return True
         
-        return isValid(root, float('-inf'), float('inf'))
+        def dfs(root, minVal, maxVal):
+            if not root: return True
+            if not (minVal<root.val<maxVal):
+                return False
+            return dfs(root.left, minVal,root.val) and dfs(root.right,root.val,maxVal)
+        return dfs(root, float("-inf"), float("inf"))
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#      minVal=float('-inf')
+#         maxVal=float('inf')
+        
+#         def dfs(root, minVal, maxVal):
+#             if not root:
+#                 return True
+            
+#             if not (root.val > minVal and root.val < maxVal):
+#                 return False
+#             return dfs(root.left, minVal, root.val) and dfs(root.right, root.val, maxVal)
+    
+#         return dfs(root, minVal, maxVal)
+        
+        
+        
