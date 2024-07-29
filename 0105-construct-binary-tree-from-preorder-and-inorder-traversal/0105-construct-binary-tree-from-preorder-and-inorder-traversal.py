4707a40694403d1b261ea54ca4e5e1aaa39c2d97
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-07-30 02:58:13 +0300

    Time: 138 ms (27.21%), Space: 85.6 MB (21.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,16 +1,48 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if len(preorder)==0 or len(inorder)==0:
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
             return
         val=preorder[0]
         index=inorder.index(val)
         root=TreeNode(val)
-        root.left=self.buildTree(preorder[1:index+1], inorder[:index])
-        root.right=self.buildTree(preorder[index+1:], inorder[index+1:])
+        root.left=self.buildTree(preorder[1:index+1],inorder[:index])
+        root.right=self.buildTree(preorder[index+1:],inorder[index+1:])
         return root
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not preorder or not inorder:
+#             return
+#         curVal = preorder[0]
+#         index= inorder.index(curVal)
+#         root=TreeNode(curVal)
+#         root.left= self.buildTree(preorder[1: index+1], inorder[:index])
+#         root.right=self.buildTree(preorder[index+1:], inorder[index+1:])
+#         return root
+        
+        
