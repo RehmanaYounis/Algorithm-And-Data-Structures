04603ae6bed03c171c0b12901df64f5fccb116ae
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-07-31 02:53:44 +0300

    Time: 257 ms (42.87%), Space: 27.8 MB (8.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,59 @@
 class Solution(object):
     def numIslands(self, grid):
-        count=0
         rows=len(grid)
         cols=len(grid[0])
-        visit=set()
+        visited=set()
+        total=0
         def dfs(r,c):
-            if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visit or grid[r][c]=='0':
-                return
-            visit.add((r,c))
+            if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visited or grid[r][c] == "0": return
+            visited.add((r,c))
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col]=='1' and (row,col) not in visit:
-                    count+=1
-                    dfs(row,col)
-        return count
+        
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in visited and grid[r][c] =="1":
+                    total+=1
+                    dfs(r,c)
+        return total
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         count=0
+#         rows=len(grid)
+#         cols=len(grid[0])
+#         visit=set()
+#         def dfs(r,c):
+#             if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visit or grid[r][c]=='0':
+#                 return
+#             visit.add((r,c))
+#             dfs(r+1,c)
+#             dfs(r-1,c)
+#             dfs(r,c+1)
+#             dfs(r,c-1)
+#         for row in range(rows):
+#             for col in range(cols):
+#                 if grid[row][col]=='1' and (row,col) not in visit:
+#                     count+=1
+#                     dfs(row,col)
+#         return count
         
