bea614b270a3ae34a5adee326ab668307c05b963
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-01 00:46:32 +0300

    Time: 1012 ms (7.71%), Space: 13.6 MB (23.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,28 +1,92 @@
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows=len(heights)
-        cols=len(heights[0])
-        pacific=[]
-        atlantic=[]
-        grid=heights
-        def dfs(r,c,preVal, stack):
-            if r<0 or r>=rows or c<0 or c>=cols or heights[r][c]<preVal or [r,c] in stack:
-                return
-            stack.append([r,c])
-            dfs(r+1,c,grid[r][c], stack)
-            dfs(r-1,c,grid[r][c], stack)
-            dfs(r,c+1,grid[r][c], stack)
-            dfs(r,c-1,grid[r][c], stack)
-        for col in range(cols):
-            dfs(0,col,grid[0][col], pacific)
-            dfs(rows-1,col,grid[rows-1][col], atlantic)
-        for row in range(rows):
-            dfs(row,0,grid[row][0], pacific)
-            dfs(row,cols-1,grid[row][cols-1], atlantic)
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        rows, cols=len(heights), len(heights[0])
+        pac,atl=[],[]
+        def dfs(r,c,visit,maxVal):
+            if (r<0 or c<0 or r>=rows or c>=cols or [r,c] in visit or heights[r][c] < maxVal):
+                return 
+            visit.append([r,c])
+            dfs(r+1,c, visit, heights[r][c])
+            dfs(r-1,c, visit, heights[r][c])
+            dfs(r,c+1, visit, heights[r][c])
+            dfs(r,c-1, visit, heights[r][c])
+            return
+            
+            
+            
+            
+        for c in range(cols):
+            dfs(0,c,pac,heights[0][c])
+            dfs(rows-1,c,atl,heights[rows-1][c])
+        
+        for r in range(rows):
+            dfs(r,0,pac,heights[r][0])
+            dfs(r,cols-1,atl,heights[r][cols-1])
         res=[]
-        for  r in range(rows):
+        for r in range(rows):
             for c in range(cols):
-                if [r,c] in pacific and [r,c] in atlantic:
+                if [r,c] in pac and [r,c] in atl:
                     res.append([r,c])
         return res
+            
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         pgrp=set()
+#         agrp=set()
+#         res=[]
+#         rows=len(heights)
+#         cols=len(heights[0])
+        
+#         def dfs(r,c,curVal,visited):
+#             if (r<0 or c<0 or r==rows or c==cols or (r,c) in visited or heights[r][c]<curVal):
+#                 return
+#             visited.add((r,c))
+#             dfs(r+1,c,heights[r][c], visited)
+#             dfs(r-1,c,heights[r][c], visited)
+#             dfs(r,c+1,heights[r][c], visited)
+#             dfs(r,c-1,heights[r][c], visited)
+          
+         
+#         for c in range (cols):
+#             dfs(0,c,heights[0][c],pgrp)
+#             dfs(rows-1,c,heights[rows-1][c],agrp)
+            
+#         for r in range (rows):
+#             dfs(r,0, heights[r][0],pgrp)
+#             dfs(r,cols-1,heights[r][cols-1],agrp )
+        
+      
+#         for r in range (rows):
+#             for c in range(cols):
+#                 if (r,c) in pgrp and (r,c) in agrp:
+#                     res.append([r,c])
+        
+#         return res
+    
+    
