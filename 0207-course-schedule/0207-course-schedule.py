32c71a74f164ad0da748edd527af331a6c5eba8d
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-07-31 19:57:50 +0300

    Time: 67 ms (56.45%), Space: 14.8 MB (15.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,79 +1,26 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        hashMap=defaultdict(list)
-        visit=[]
-        for crs, pre in prerequisites :
-            hashMap[crs].append(pre)
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        preMap=defaultdict(list)
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
         
-        
-        def dfs(course):
-            if course in visit: return False
-            
-            if hashMap[course]==[]:
+        visit=set()
+        def dfs(crs):
+            if preMap[crs]==[]:
                 return True
-            visit.append(course)
-            for pre in hashMap[course]:
-                if not dfs(pre): return False
-            hashMap[course]=[]
-            visit.pop()
+            if crs in visit:
+                return False
+            visit.add(crs)
+            for cs in preMap[crs]:
+                if not dfs(cs):
+                    return False
+            preMap[crs]=[]
             return True
         
         
-        for course in range(numCourses):
-            if not dfs(course): 
+        for crs in range(numCourses):
+            if not dfs(crs):
                 return False
         return True
+
         
-                
-#         hashMap=defaultdict(list)
-#         for crs, pre in prerequisites:
-#             hashMap[crs].append(pre)
-#         visit=[]
-#         def dfs(crs):
-#             if crs in visit:
-#                 return False
-#             if hashMap[crs]==[]:
-#                 return True
-#             visit.append(crs)
-#             for pre in hashMap[crs]:
-#                 if not dfs(pre): 
-#                     return False
-#             visit.pop()
-#             hashMap[crs]=[]
-#             return True
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         hashMap=defaultdict(list)
-#         for crs, pre in prerequisites:
-#             hashMap[crs].append(pre)
-#         visit=[]
-#         def dfs(crs):
-#             if crs in visit:
-#                 return False
-#             if hashMap[crs]==[]:
-#                 return True
-#             visit.append(crs)
-#             for pre in hashMap[crs]:
-#                 if not dfs(pre): 
-#                     return False
-#             visit.pop()
-#             hashMap[crs]=[]
-#             return True
-#         for crs in range(numCourses):
-#             if not dfs(crs):
-#                 return False
-#         return True
-    
-    
-    
