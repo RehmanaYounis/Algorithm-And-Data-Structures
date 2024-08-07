95102c3347b0cf41d87232e94b87bffcfa53149c
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-07 17:18:08 -0400

    Time: 70 ms (83.73%), Space: 19.8 MB (91.66%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,47 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res=[]
+        nstart,nend=newInterval
+        print(nstart,nend)
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
+            start= intervals[i][0]
+            end=intervals[i][1]
+            if nend <start:
+                res.append([nstart,nend])
                 res+=intervals[i:]
                 return res
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
+            elif end < nstart:
+                res.append([start,end])
             else:
-                newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
-        res.append(newInterval)
+                nstart=min(nstart,start)
+                nend= max(nend,end)
+        res.append([nstart,nend])
         return res
-      
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res=[]
-        
-#         newStart=newInterval[0]
-#         newEnd=newInterval[1]
-#         for i in range(len(intervals)):
-#             start=intervals[i][0]
-#             end=intervals[i][1]
-            
-#             if newEnd<start:
-#                 res.append([newStart, newEnd])
-#                 return res+ intervals[i:]
                 
-#             if end<newStart:
-#                 res.append([start, end])
-#             else:
-#                 newStart=min(newStart, start)
-#                 newEnd=max(newEnd, end)
-#         res.append([newStart, newEnd])
-#         return res
+            
