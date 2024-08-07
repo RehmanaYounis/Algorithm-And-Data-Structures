f7fe921f0b9afc4d4d3b9881c0ad1325050a75a2
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-07 17:33:05 -0400

    Time: 130 ms (24.34%), Space: 21.2 MB (22.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,57 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda i : i[0])
-        
-        stack=[]
-        stack.append(intervals[0])
-        for i in range(1, len(intervals)):
-            if stack[-1][1] >= intervals[i][0]:
-                stack[-1][1]=max(stack[-1][1], intervals[i][1])   
-            else:
-                stack.append(intervals[i])
-        return stack       
-    
-
-#         for start, end in intervals:
-#             lastEnd = output[-1][1]
-
-#             if start <= lastEnd:
-#                 # merge
-#                 output[-1][1] = max(lastEnd, end)
-#             else:
-#                 output.append([start, end])
-#         return output
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         stack=[]
-#         intervals.sort(key= lambda i:i[0])
-#         stack.append(intervals[0])
-#         for i in range(1, len(intervals)):
-#             start=intervals[i][0]
-#             end=intervals[i][1]
-#             if start <= stack[-1][1]:
-#                 newStart, newEnd=stack.pop()
-#                 newStart=min(newStart, start)
-#                 newEnd=max(newEnd, end)
-#                 stack.append([newStart, newEnd])
-#             else:
-#                 stack.append([start,end])
+        intervals.sort(key = lambda i:i[0])
+        res=[intervals[0]]
+        for start, end in intervals:
+            cur_start=res[-1][0]
+            cur_end=res[-1][1]
             
-#         return stack        
+            if cur_end<start:
+                res.append([start, end])
+            else:
+                res[-1][0]=min(start,cur_start)
+                res[-1][1]=max(end, cur_end)
+            
+        return res
+        
