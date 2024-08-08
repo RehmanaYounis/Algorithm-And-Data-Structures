67257f8a6eb4862393ad10060c9d681ceda7fc3a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-07 20:03:47 -0400

    Time: 78 ms (38.79%), Space: 19.9 MB (14.26%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -2,20 +2,17 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res=[]
         nstart,nend=newInterval
-        print(nstart,nend)
         for i in range(len(intervals)):
-            start= intervals[i][0]
-            end=intervals[i][1]
-            if nend <start:
-                res.append([nstart,nend])
+            start,end=intervals[i][0],intervals[i][1]
+            if nend<start:
+                res.append([nstart, nend])
                 res+=intervals[i:]
                 return res
-            elif end < nstart:
-                res.append([start,end])
+            elif end<nstart:
+                res.append(intervals[i])
             else:
-                nstart=min(nstart,start)
-                nend= max(nend,end)
+                nstart=min(nstart, start)
+                nend=max(nend, end)
         res.append([nstart,nend])
         return res
-                
             
