08bbb485ca823c78c5f1ac00b62a0ec4d6fa609b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-08 18:32:38 -0400

    Time: 102 ms (11.94%), Space: 12.9 MB (7.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,34 +1,30 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        ROWS= len(matrix)
-        COLS=len(matrix[0])
-        visit=set()
-        def dfs(r,c,dire):
-            if r<0 or r>=ROWS or c<0 or c>= COLS  or matrix[r][c] == 0:
+class Solution(object):
+    def setZeroes(self, matrix):
+        rows, cols=len(matrix), len(matrix[0])
+        
+        def dfs(r,c,dir):
+            if r<0 or r>=rows or c<0 or c>=cols or matrix[r][c]== 0:
                 return
             matrix[r][c]='*'
-            if dire == 'U':
-                dfs(r-1, c, 'U')
-            elif dire == 'D':
-                dfs(r+1, c, 'D')
-            elif dire =='R':
-                dfs(r, c+1,'R')
-            else:
-                dfs(r, c-1,'L')
+            if dir=='U':
+                dfs(r-1,c,'U')
+            if dir=='D':
+                dfs(r+1,c,'D')
+            if dir=='L':
+                dfs(r,c-1,'L')
+            if dir=='R':
+                dfs(r,c+1,'R')
         
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 if matrix[r][c]==0:
-                    dfs(r+1,c, 'D')
-                    dfs(r-1,c, 'U')
-                    dfs(r,c-1, 'L')
-                    dfs(r,c+1, 'R')
-        
-        
-        for r in range(ROWS):
-            for c in range(COLS):
+                    dfs(r-1,c,'U')
+                    dfs(r+1,c,'D')
+                    dfs(r,c-1,'L')
+                    dfs(r,c+1,'R')
+        for r in range(rows):
+            for c in range(cols):
                 if matrix[r][c]=='*':
                     matrix[r][c]=0
-                    
         return matrix
-        
+                    
