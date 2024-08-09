76e964941a951a3e84ff65f1505716768947a21b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-09 16:19:37 -0400

    Time: 102 ms (61.31%), Space: 17.5 MB (15.43%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,30 +1,98 @@
-class Solution(object):
-    def setZeroes(self, matrix):
-        rows, cols=len(matrix), len(matrix[0])
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows=len(matrix)
+        cols=len(matrix[0])
         
-        def dfs(r,c,dir):
-            if r<0 or r>=rows or c<0 or c>=cols or matrix[r][c]== 0:
+        def dfs(r,c,dire):
+            if r <0 or r>=rows or c < 0 or c>=cols or matrix[r][c] == 0:
                 return
-            matrix[r][c]='*'
-            if dir=='U':
+            matrix[r][c] = '*'
+            if dire=='U':
                 dfs(r-1,c,'U')
-            if dir=='D':
+            if dire=='D':
                 dfs(r+1,c,'D')
-            if dir=='L':
+            if dire=='L':
                 dfs(r,c-1,'L')
-            if dir=='R':
+            if dire=='R':
                 dfs(r,c+1,'R')
-        
+                
         for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c]==0:
+            for c in range (cols):
+                if matrix[r][c] == 0:
                     dfs(r-1,c,'U')
                     dfs(r+1,c,'D')
                     dfs(r,c-1,'L')
                     dfs(r,c+1,'R')
+        
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c]=='*':
                     matrix[r][c]=0
         return matrix
                     
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ROWS= len(matrix)
+#         COLS=len(matrix[0])
+#         visit=set()
+#         def dfs(r,c,dire):
+#             if r<0 or r>=ROWS or c<0 or c>= COLS  or matrix[r][c] == 0:
+#                 return
+#             matrix[r][c]='*'
+#             if dire == 'U':
+#                 dfs(r-1, c, 'U')
+#             elif dire == 'D':
+#                 dfs(r+1, c, 'D')
+#             elif dire =='R':
+#                 dfs(r, c+1,'R')
+#             else:
+#                 dfs(r, c-1,'L')
+        
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if matrix[r][c]==0:
+#                     dfs(r+1,c, 'D')
+#                     dfs(r-1,c, 'U')
+#                     dfs(r,c-1, 'L')
+#                     dfs(r,c+1, 'R')
+        
+        
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if matrix[r][c]=='*':
+#                     matrix[r][c]=0
+                    
+#         return matrix
+        
