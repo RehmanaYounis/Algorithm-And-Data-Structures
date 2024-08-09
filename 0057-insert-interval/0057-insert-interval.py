595c41bd5d3289ffee1f4a600ad307f91c0d761b
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-09 18:09:56 -0400

    Time: 53 ms (46.39%), Space: 15.2 MB (28.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,18 +1,67 @@
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+class Solution(object):
+    def insert(self, intervals, newInterval):
         res=[]
-        nstart,nend=newInterval
+        new_start, new_end=newInterval
         for i in range(len(intervals)):
-            start,end=intervals[i][0],intervals[i][1]
-            if nend<start:
-                res.append([nstart, nend])
+            cur_start=intervals[i][0]
+            cur_end=intervals[i][1]
+            
+            
+            if new_end < cur_start:
+                res.append([new_start,new_end])
                 res+=intervals[i:]
                 return res
-            elif end<nstart:
-                res.append(intervals[i])
-            else:
-                nstart=min(nstart, start)
-                nend=max(nend, end)
-        res.append([nstart,nend])
-        return res
             
+            elif cur_end < new_start:
+                res.append(intervals[i])
+            
+            else:
+                new_start=min(new_start, cur_start)
+                new_end=max(new_end,cur_end)
+                
+        res.append([new_start, new_end])
+        return res
+             
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res=[]
+#         for i in range(len(intervals)):
+#             if newInterval[1]<intervals[i][0]:
+#                 res.append(newInterval)
+#                 res+=intervals[i:]
+#                 return res
+#             elif newInterval[0]>intervals[i][1]:
+#                 res.append(newInterval)
+#             else:
+#                 newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+#         res.append(newInterval)
+#         return res
+                
+        
