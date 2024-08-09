4ab302cf1bd7e7bc8efff0b46c1285dda7ebec0c
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-09 18:46:08 -0400

    Time: 103 ms (44.51%), Space: 17.6 MB (27.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,7 +4,7 @@ class Solution(object):
         intervals.sort(key=lambda i:i[0])
         res=[intervals[0]]
         prev = intervals[0][1]
-        for i in range(len(intervals)):
+        for i in range(1, len(intervals)):
             cur_start=intervals[i][0]
             cur_end=intervals[i][1]
             
