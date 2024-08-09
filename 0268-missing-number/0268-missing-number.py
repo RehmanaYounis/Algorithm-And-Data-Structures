4b207cd4d570f229db73ce048cd9f60977c9c5e9
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-09 15:18:29 -0400

    Time: 1360 ms (12.66%), Space: 12.9 MB (64.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,5 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        size=len(nums)
-        for i in range(size+1):
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)+1):
             if i not in nums:
                 return i
-        
