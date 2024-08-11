de0d67e90a3aba4cf4daafad80cfc58b88ae442a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-11 19:26:09 -0400

    Time: 47 ms (30.14%), Space: 11.7 MB (70.46%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,56 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
-        res, curSol=[],[]
-        curSum=0
-        nums=candidates
-        length=len(candidates)
-        i=0
-        def dfs(i,curSum):
-            if curSum == target:
-                res.append(curSol[:])
-                return 
-            if curSum> target or i == length:
+        res=[]
+        curSol=[]
+        def dfs(i, curVal):
+            if i>= len(candidates) or curVal>target:
                 return
-            dfs(i+1, curSum)
-            curSol.append(nums[i])
-            dfs(i, curSum+nums[i])
+            if curVal==target:
+                res.append(curSol[:])
+                return
+            dfs(i+1, curVal)
+            curSol.append(candidates[i])
+            dfs(i, curVal+candidates[i])
             curSol.pop()
         dfs(0,0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res, curSol=[],[]
+#         curSum=0
+#         nums=candidates
+#         length=len(candidates)
+#         i=0
+#         def dfs(i,curSum):
+#             if curSum == target:
+#                 res.append(curSol[:])
+#                 return 
+#             if curSum> target or i == length:
+#                 return
+#             dfs(i+1, curSum)
+#             curSol.append(nums[i])
+#             dfs(i, curSum+nums[i])
+#             curSol.pop()
+#         dfs(0,0)
+#         return res
             
         
         
