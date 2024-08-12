bc827fbc2d40687f6a4c9d76b4a1758cd60891c2
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-12 18:27:08 -0400

    Time: 10 ms (97.79%), Space: 11.8 MB (78.00%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,23 +1,46 @@
 class Solution(object):
     def subsetsWithDup(self, nums):
         res=[]
-        stack=[]
+        visit=[]
         nums.sort()
         def dfs(i):
-            if i>=len(nums):
-                res.append(stack[::])
+            if i == len(nums):
+                res.append(visit[:])
                 return
-            
-            stack.append(nums[i])
+            visit.append(nums[i])
             dfs(i+1)
-            while i+1<len(nums) and nums[i] == nums[i+1]:
+            while i+1 < len(nums) and nums[i+1] == nums[i]:
                 i+=1
-            stack.pop()
-
+            visit.pop()
             dfs(i+1)
+        dfs(0)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res=[]
+#         stack=[]
+#         nums.sort()
+#         def dfs(i):
+#             if i>=len(nums):
+#                 res.append(stack[::])
+#                 return
             
 #             stack.append(nums[i])
 #             dfs(i+1)
+#             while i+1<len(nums) and nums[i] == nums[i+1]:
+#                 i+=1
+#             stack.pop()
+#             dfs(i+1)
+            
+# #             stack.append(nums[i])
+# #             dfs(i+1)
             
 #             stack.pop()
 #             while i+1<len(nums) and nums[i] == nums[i+1]:
