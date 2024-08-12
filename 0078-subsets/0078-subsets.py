53b1a4081c189fa5a2a785bfed5bb144daba59ae
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-08-12 17:21:32 -0400

    Time: 28 ms (95.81%), Space: 16.6 MB (60.25%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,45 @@
 class Solution(object):
     def subsets(self, nums):
         res=[]
-        def dfs(n, stack):
-            if n>= len(nums):
-                res.append(stack[::])
+        visit=[]
+        def dfs(i):
+            if i >= len(nums) or nums[i] in visit:
+                res.append(visit[:])
                 return
-            stack.append(nums[n])
-            dfs(n+1, stack)
-            stack.pop()
-            dfs(n+1, stack)
-        dfs(0,[])
+            
+            dfs(i+1)
+            visit.append(nums[i])
+            dfs(i+1)
+            visit.pop()
+        dfs(0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res=[]
+#         def dfs(n, stack):
+#             if n>= len(nums):
+#                 res.append(stack[::])
+#                 return
+#             stack.append(nums[n])
+#             dfs(n+1, stack)
+#             stack.pop()
+#             dfs(n+1, stack)
+#         dfs(0,[])
+#         return res
