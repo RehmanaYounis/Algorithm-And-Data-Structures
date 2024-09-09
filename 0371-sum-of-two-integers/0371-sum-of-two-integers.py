da2e4db4169aabf48a464001b5ab59d6f13aa351
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-09-09 06:51:51 -0400

    Time: 29 ms (87.13%), Space: 16.5 MB (16.71%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,9 +1,19 @@
-class Solution(object):
-    def getSum(self, a, b):
-        bitShortner = 0xffffffff
-        while ((bitShortner &b)>0):
-            temp = int( (a&b) << 1)
-            a= a ^ b
-            b=temp
+#https://www.youtube.com/watch?v=_pUidg9gQyA
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        bitShortner=0xffffffff
+        while ((b&bitShortner)>0):
+            carry=int((a & b)<<1)
+            a=a^b
+            b=carry
         return (a & bitShortner) if b>0 else a
+        
+        
+        # def getSum(self, a, b):
+        # bitShortner = 0xffffffff
+        # while ((bitShortner &b)>0):
+        #     temp = int( (a&b) << 1)
+        #     a= a ^ b
+        #     b=temp
+        # return (a & bitShortner) if b>0 else a
         
