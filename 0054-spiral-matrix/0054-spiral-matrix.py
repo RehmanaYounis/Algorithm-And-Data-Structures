8eb79fcd5338cf1fae62fb1d2de746bfdafb0b8f
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-09-10 11:08:05 -0400

    Time: 38 ms (36.61%), Space: 16.5 MB (78.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,30 +1,82 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        row_start=0
-        rows= len(matrix)
-        col_start =0
-        cols = len(matrix[0])
-        
+        left, right=0, len(matrix[0])
+        top, bottom= 0, len(matrix)
         res=[]
-        while row_start < rows and col_start <cols:
-            for c in range(col_start, cols):
-                res.append(matrix[row_start][c])
-        
-            row_start+=1
-         
-            for r in range(row_start, rows):
-                res.append(matrix[r][cols-1])
-            cols-=1
-            
-            if row_start < rows:
-                for c in range(cols-1, col_start-1, -1):
-                    res.append(matrix[rows-1][c])
-                rows-=1
-            if col_start<cols:
-                for r in range(rows-1, row_start-1, -1):
-                    res.append(matrix[r][col_start])
-                col_start+=1
+        while left <right and top < bottom:
+            for col in range(left,right):
+                res.append(matrix[top][col])
+            top+=1
+            for row in range(top, bottom):
+                res.append(matrix[row][right-1])
+           
+            right-=1
+            if top <bottom:
+                for col in range(right-1, left-1, -1):
+                    res.append(matrix[bottom-1][col])
+                bottom-=1
+            if left<right:
+                for row in range(bottom-1, top-1, -1):
+                    res.append(matrix[row][left])
+                left+=1
         return res
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         row_start=0
+#         rows= len(matrix)
+#         col_start =0
+#         cols = len(matrix[0])
+        
+#         res=[]
+#         while row_start < rows and col_start <cols:
+#             for c in range(col_start, cols):
+#                 res.append(matrix[row_start][c])
+        
+#             row_start+=1
+         
+#             for r in range(row_start, rows):
+#                 res.append(matrix[r][cols-1])
+#             cols-=1
+            
+#             if row_start < rows:
+#                 for c in range(cols-1, col_start-1, -1):
+#                     res.append(matrix[rows-1][c])
+#                 rows-=1
+#             if col_start<cols:
+#                 for r in range(rows-1, row_start-1, -1):
+#                     res.append(matrix[r][col_start])
+#                 col_start+=1
+#         return res
         
         
         
