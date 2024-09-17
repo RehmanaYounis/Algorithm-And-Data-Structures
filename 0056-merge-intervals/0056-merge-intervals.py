4961a41994f5958cb761dfc5e059b922475a4839
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-09-17 19:34:48 -0400

    Time: 214 ms (5.12%), Space: 16.7 MB (47.81%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,20 +2,68 @@ class Solution(object):
     def merge(self, intervals):
         intervals.sort()
         res=[]
-        curS,curE=intervals[0]
-        res.append([curS,curE])
+        res.append(intervals[0])
+        
         for i in range(1,len(intervals)):
-            curS,curE=res[-1]
-            nextS,nextE=intervals[i]
-            if curE<nextS:
-                res.append([nextS,nextE])
-                curS=nextS
-                curE=nextE
+            prevS,prevE=res[-1][0], res[-1][1]
+            curS,curE=intervals[i][0], intervals[i][1]
+            if prevE<curS:
+                res.append(intervals[i])
             else:
-                res[-1][0]=min(res[-1][0], nextS)
-                res[-1][1]=max(res[-1][1],nextE)
-            
+                res[-1][0], res[-1][1]=min(prevS,curS), max(prevE,curE)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         intervals.sort()
+#         res=[]
+#         curS,curE=intervals[0]
+#         res.append([curS,curE])
+#         for i in range(1,len(intervals)):
+#             curS,curE=res[-1]
+#             nextS,nextE=intervals[i]
+#             if curE<nextS:
+#                 res.append([nextS,nextE])
+#                 curS=nextS
+#                 curE=nextE
+#             else:
+#                 res[-1][0]=min(res[-1][0], nextS)
+#                 res[-1][1]=max(res[-1][1],nextE)
+            
+#         return res
         
         
         
