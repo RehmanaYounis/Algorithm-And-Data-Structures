066513598be8f1b33afad3cc03f8253c2ea91983
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-09-26 19:32:33 -0400

    Time: 65 ms (6.50%), Space: 17.2 MB (44.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,70 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        def isAlphnum(c):
-            if ('A'<= c <= 'Z' or
-               'a'<= c <= 'z' or
-                '0'<= c <= '9'):
-                return True
-            else:
-                return False
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         s=s.lower()
-        l=0
-        r=len(s) -1
-        while l<r:
-            while l<r and not isAlphnum(s[l]):
-                l+=1
-            while l<r and not isAlphnum(s[r]):
-                r-=1
-            if s[l] != s[r]:
+        left,right=0,len(s)-1
+        while left <=right:
+            while left<right and not self.isChar(s[left]):
+                left+=1
+            while left<right and not self.isChar(s[right]):
+                right-=1
+            # print(left, right, s[left],s[right])
+            if self.isChar(s[left]) and self.isChar(s[right]) and s[right]!= s[left]:
                 return False
-            l+=1
-            r-=1
+            else:
+                left+=1
+                right-=1
         return True
-                    
-            
-            
         
+    def isChar(self,c):
+        if (ord('0') <= ord(c) <= ord('9')) or (ord('a') <= ord(c) <= ord('z')) or (ord('A') <= ord(c) <= ord('Z')):
+            return True
+        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         s=s.lower()
+#         def isAlpha(c):
+#             if ord('a')<= ord(c) <= ord('z') or ord('0') <=ord(c) <= ord('9'): return True
+#             return False
+#         l=0
+#         r=len(s)-1
+        
+#         while l<r:
+#             while l<r and not isAlpha(s[l]):
+#                     l+=1
+#             while l<r and not isAlpha(s[r]):
+#                     r-=1
+#             if s[l] != s[r]:
+#                 return False
+#             l+=1
+#             r-=1
+#         return True
+                
