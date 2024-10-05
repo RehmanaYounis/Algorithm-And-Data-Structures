ffe134896ea1cb13668c68cc0e4712ec86b1a7da
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-04 20:39:44 -0400

    Time: 381 ms (47.14%), Space: 38.7 MB (33.88%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,52 +1,42 @@
 class MedianFinder:
+
     def __init__(self):
-        self.small,self.large=[],[]
+        self.left,self.right=[],[]
         
 
     def addNum(self, num: int) -> None:
-        if self.large and num>self.large[0]:
-            heapq.heappush(self.large,num)
+        if self.right and num>self.right[0]:
+            heapq.heappush(self.right,num)
         else:
-            heapq.heappush(self.small, (-1 * num))
-        if len(self.small)> len(self.large)+1:
-            val=-1* heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-            
-        elif len(self.large)> len(self.small)+1:
-            val=heapq.heappop(self.large)
-            heapq.heappush(self.small, (-1 * val))
+            heapq.heappush(self.left, -num)
+        
+        if len(self.left)>len(self.right)+1:
+            val=-1* heappop(self.left)
+            heapq.heappush(self.right,val)
+        
+        elif len(self.right) > len(self.left)+1:
+            val=-1 * heappop(self.right)
+            heapq.heappush(self.left,val)
 
+        
 
+#         elif len(self.large)> len(self.small)+1:
+#             val=heapq.heappop(self.large)
+#             heapq.heappush(self.small, (-1 * val))
+        
 
-#         if len(self.small) > len(self.large) + 1:
-#             val = -1 * heapq.heappop(self.small)
-#             heapq.heappush(self.large, val)
-#         if len(self.large) > len(self.small) + 1:
-#             val = heapq.heappop(self.large)
-#             heapq.heappush(self.small, -1 * val)       
-            
-            
-            
-            
     def findMedian(self) -> float:
-        if len(self.small)>len(self.large):
-            val = -1 * self.small[0]
-        elif len(self.large)>len(self.small):
-            val = self.large[0]
+        if len(self.left) > len(self.right):
+            return -1 * self.left[0]
+        elif len(self.right) > len(self.left):
+            return self.right[0]
         else:
-            v1=-1 * self.small[0]
-            v2= self.large[0]
-            val=(v1+v2)/2
-        return val
+            val1=-1 * self.left[0]
+            val2= self.right[0]
+            return (val1+val2)/2
 
-       
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
