218034701ecb9e77d052b9ce3e5600a90d8a827a
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-05 18:18:36 -0400

    Time: 33 ms (73.55%), Space: 16.7 MB (10.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         rows,cols=m,n
-        @cache
+        dp={}
         def dfs(r,c):
+            if (r,c) in dp:
+                return dp[(r,c)]
             if r< 0 or r >=rows or c< 0 or c>=cols:
                 return 0
             if r==(m-1) and c ==(n-1):
                 return 1
-            return dfs(r+1,c)+dfs(r,c+1)
+            dp[(r,c)]=dfs(r+1,c)+dfs(r,c+1)
+            return dp[(r,c)]
         return dfs(0,0)
             
         
