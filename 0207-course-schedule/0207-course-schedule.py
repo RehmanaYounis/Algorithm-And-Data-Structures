2c5bacb6cb191adb50736710ffa967fc39d592d9
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-05 18:40:39 -0400

    Time: 68 ms (71.27%), Space: 14.8 MB (20.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,75 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        preMap=defaultdict(list)
+        crsMap=defaultdict(list)
         for crs, pre in prerequisites:
-            preMap[crs].append(pre)
-        
-        visit=set()
+            crsMap[crs].append(pre)
+        visited=set()
         def dfs(crs):
-            if preMap[crs]==[]:
+            if crs in visited:
+                return False
+            if crsMap[crs]==[]:
                 return True
-            if crs in visit:
-                return False
-            visit.add(crs)
-            for cs in preMap[crs]:
-                if not dfs(cs):
-                    return False
-            preMap[crs]=[]
+            visited.add(crs)
+            for pre in crsMap[crs]:
+                if not dfs(pre): return False
+            visited.remove(crs)
+            crsMap[crs]=[]
             return True
-        
-        
         for crs in range(numCourses):
-            if not dfs(crs):
-                return False
+            if not dfs(crs): return False
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         preMap=defaultdict(list)
+#         for crs, pre in prerequisites:
+#             preMap[crs].append(pre)
+        
+#         visit=set()
+#         def dfs(crs):
+#             if preMap[crs]==[]:
+#                 return True
+#             if crs in visit:
+#                 return False
+#             visit.add(crs)
+#             for cs in preMap[crs]:
+#                 if not dfs(cs):
+#                     return False
+#             preMap[crs]=[]
+#             return True
+        
+        
+#         for crs in range(numCourses):
+#             if not dfs(crs):
+#                 return False
+#         return True
 
         
