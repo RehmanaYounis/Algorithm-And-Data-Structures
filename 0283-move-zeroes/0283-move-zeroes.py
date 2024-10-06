6356ce7daa4848b9a955a95b99b9a2ddeec846f2
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-06 17:57:08 -0400

    Time: 128 ms (47.21%), Space: 18 MB (67.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,8 +1,9 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        l,r=0,0
-        while l<len(nums) and r <len(nums):
-            if nums[r] !=0:
-                nums[l],nums[r]=nums[r],nums[l]
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l=0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l],nums[r] =nums[r],nums[l]
                 l+=1
-            r+=1
+        
+        
