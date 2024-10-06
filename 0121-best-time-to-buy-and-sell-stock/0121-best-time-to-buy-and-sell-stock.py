46178f6151558d9bf8db2857a7b43465b055b753
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-06 18:27:42 -0400

    Time: 790 ms (40.38%), Space: 20.8 MB (66.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,104 +1,12 @@
 class Solution(object):
     def maxProfit(self, prices):
-        l,r=0,1
+        l=0
+        r=1
         maxProf=0
-        nums=prices
-        while r<len(prices):
-            curProf=nums[r]-nums[l]
-            maxProf=max(maxProf,curProf)
-            if nums[r]<nums[l]:
+        while r< len(prices):
+            if prices[r] < prices[l]:
                 l=r
-                r+=1
-            else:
-                r+=1
+            maxProf=max(maxProf, (prices[r]-prices[l]))
+            r+=1
         return maxProf
             
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         l=0
-#         r=1
-#         maxProfit=0
-#         while r<len(prices):
-#             curProf=prices[r]-prices[l]
-#             maxProfit=max(maxProfit,curProf)
-#             if prices[r]<prices[l]:
-#                 l=r
-#                 r+=1
-#             else:
-#                 r+=1
-#         return maxProfit
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         l=0
-#         r=1
-#         maxProfit=0
-#         nums=prices
-#         while r<len(nums):
-#             curProfit = nums[r]-nums[l]
-#             maxProfit = max(maxProfit , curProfit)
-#             if nums[l]>nums[r]:
-#                 l=r
-#             r=r+1
-#         return maxProfit
