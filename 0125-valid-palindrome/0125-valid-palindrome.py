86a0b9588bea4e557efc3ca0cb3a82e85fabaa3d
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-06 17:47:22 -0400

    Time: 59 ms (9.56%), Space: 17 MB (85.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,75 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower()
-        left,right=0,len(s)-1
-        while left <=right:
-            while left<right and not self.isChar(s[left]):
-                left+=1
-            while left<right and not self.isChar(s[right]):
-                right-=1
-            # print(left, right, s[left],s[right])
-            if self.isChar(s[left]) and self.isChar(s[right]) and s[right]!= s[left]:
+        def isvalidCh(ch):
+            if (ord('0') <= ord(ch) <= ord('9')) \
+            or (ord('a') <= ord(ch) <= ord('z')) \
+            or (ord('A') <= ord(ch) <= ord('Z')):
+                return True
+            return False
+        s= s.lower()
+        l, r = 0, len(s)-1
+        while l<=r:
+            while l<=r and not isvalidCh(s[l]):
+                l+=1
+            while l<=r and not isvalidCh(s[r]):
+                r-=1
+            if l<r and s[l]!=s[r]:
                 return False
             else:
-                left+=1
-                right-=1
+                l+=1
+                r-=1
         return True
         
-    def isChar(self,c):
-        if (ord('0') <= ord(c) <= ord('9')) or (ord('a') <= ord(c) <= ord('z')) or (ord('A') <= ord(c) <= ord('Z')):
-            return True
-        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         s=s.lower()
+#         left,right=0,len(s)-1
+#         while left <=right:
+#             while left<right and not self.isChar(s[left]):
+#                 left+=1
+#             while left<right and not self.isChar(s[right]):
+#                 right-=1
+#             # print(left, right, s[left],s[right])
+#             if self.isChar(s[left]) and self.isChar(s[right]) and s[right]!= s[left]:
+#                 return False
+#             else:
+#                 left+=1
+#                 right-=1
+#         return True
+        
+#     def isChar(self,c):
+#         if (ord('0') <= ord(c) <= ord('9')) or (ord('a') <= ord(c) <= ord('z')) or (ord('A') <= ord(c) <= ord('Z')):
+#             return True
+#         return False
         
         
         
