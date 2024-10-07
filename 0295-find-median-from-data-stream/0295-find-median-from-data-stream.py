d5a41017d79aa0b627d80fc9d657cebbebed9a10
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-06 20:57:43 -0400

    Time: 374 ms (56.39%), Space: 38.8 MB (18.77%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,37 +5,31 @@ class MedianFinder:
         
 
     def addNum(self, num: int) -> None:
-        if self.right and num>self.right[0]:
-            heapq.heappush(self.right,num)
-        else:
-            heapq.heappush(self.left, -num)
-        
+        if len(self.right) >= 1 and num>self.right[0]:
+            heapq.heappush(self.right, num)
+        else: 
+            heapq.heappush(self.left, -1*num)
         if len(self.left)>len(self.right)+1:
-            val=-1* heappop(self.left)
+            val=-1*heapq.heappop(self.left)
             heapq.heappush(self.right,val)
-        
-        elif len(self.right) > len(self.left)+1:
-            val=-1 * heappop(self.right)
+        if len(self.right)>len(self.left)+1:
+            val=-1*heapq.heappop(self.right)
             heapq.heappush(self.left,val)
-
-        
-
-#         elif len(self.large)> len(self.small)+1:
-#             val=heapq.heappop(self.large)
-#             heapq.heappush(self.small, (-1 * val))
-        
-
+            
+            
     def findMedian(self) -> float:
-        if len(self.left) > len(self.right):
-            return -1 * self.left[0]
-        elif len(self.right) > len(self.left):
+        if len(self.right) > len(self.left):
             return self.right[0]
+        elif len(self.left) > len(self.right):
+            return -1* self.left[0]
         else:
-            val1=-1 * self.left[0]
-            val2= self.right[0]
-            return (val1+val2)/2
+            val1=self.right[0]
+            val2=-1*self.left[0]
+            return (val1 + val2)/2
+                 
 
-
+      
+            
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
