ea894e16f86b9a6dd2b16177507d941df6e84a7f
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-09 16:56:46 -0400

    Time: 103 ms (58.00%), Space: 17.7 MB (10.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,115 +1,18 @@
 class Solution(object):
     def merge(self, intervals):
-        intervals.sort()
-        res=[]
-        res.append(intervals[0])
-        
-        for i in range(1,len(intervals)):
-            prevS,prevE=res[-1][0], res[-1][1]
-            curS,curE=intervals[i][0], intervals[i][1]
-            if prevE<curS:
-                res.append(intervals[i])
+        intervals=sorted(intervals)
+        prev=intervals[0][1]
+        stack=[intervals[0]]
+        for i in range(1, len(intervals)):
+            start=intervals[i][0]
+            end=intervals[i][1]
+            if prev >= start:
+                stack[-1][0]=min(stack[-1][0],start)                
+                stack[-1][1]=max(prev,end)
             else:
-                res[-1][0], res[-1][1]=min(prevS,curS), max(prevE,curE)
-        return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         intervals.sort()
-#         res=[]
-#         curS,curE=intervals[0]
-#         res.append([curS,curE])
-#         for i in range(1,len(intervals)):
-#             curS,curE=res[-1]
-#             nextS,nextE=intervals[i]
-#             if curE<nextS:
-#                 res.append([nextS,nextE])
-#                 curS=nextS
-#                 curE=nextE
-#             else:
-#                 res[-1][0]=min(res[-1][0], nextS)
-#                 res[-1][1]=max(res[-1][1],nextE)
+                stack.append([start,end])
+            prev=stack[-1][1]
+        return stack
+
             
-#         return res
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
-#         intervals.sort(key=lambda i:i[0])
-#         res=[intervals[0]]
-#         prev = intervals[0][1]
-#         for i in range(1, len(intervals)):
-#             cur_start=intervals[i][0]
-#             cur_end=intervals[i][1]
-            
-#             if prev < cur_start:
-#                 res.append([cur_start,cur_end])
-#             else:
-#                 res[-1][1]=max(cur_end,prev )
-                
-#             prev=max(prev, cur_end)
-            
-#         return res
-            
-                
-                
