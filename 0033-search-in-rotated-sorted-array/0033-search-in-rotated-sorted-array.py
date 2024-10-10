beefcb385f16d4ecd539b5c38f03b8cb1e9fdef5
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-10 16:48:11 -0400

    Time: 48 ms (22.11%), Space: 16.9 MB (38.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,56 +1,28 @@
-# [4,5,6,7,0,1,2]
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # find pivot point
-        if len(nums) ==0: return -1
-        l,r=0, len(nums)-1
+        #find the pivot
+        l,r=0,len(nums)-1
+        
         while l<r:
-            mid=(l+r)//2
-            if nums[mid] > nums[r]:
-                l=mid+1
+            m=(l+r)//2
+            if nums[m]>nums[r]:
+                l=m+1
             else:
-                r=mid
-            
+                r=m
+        
         pivot=l
-        l,r=0, len(nums)-1
-        if target >=nums[pivot] and target <=nums[r]:
+        l,r=0,len(nums)-1
+        if target >= nums[pivot] and target <= nums[r]:
             l=pivot
-        elif target >= nums[l] and target <=nums[pivot-1]:
+        else:
             r=pivot-1
-        print(l,r)
+        
         while l<=r:
-            mid=(l+r)//2
-            if target < nums[mid]:
-                r=mid-1
-            elif target >nums[mid]:
-                l=mid+1
+            m=(l+r)//2
+            if target>nums[m]:
+                l=m+1
+            elif target <nums[m]:
+                r=m-1
             else:
-                return mid
+                return m
         return -1
-            
-                
-# if len(nums) == 1 and target == nums[0]: return 0
-#         if len(nums) == 1 and target != nums[0]: return -1
-#         l,r=0,len(nums)-1
-#         while l<r:
-#             mid=(l+r)//2
-#             if nums[mid]>nums[r]:
-#                 l=mid+1
-#             else:
-#                 r=mid
-#         pivot=l
-#         l,r=0,len(nums)-1
-#         if target >= nums[pivot] and target <= nums[r]:
-#             l = pivot
-#         else:
-#             r = pivot - 1
-#         print("boundries are", l,r)    
-#         while l<=r:
-#             mid=(l+r)//2
-#             if target == nums[mid]:
-#                 return mid
-#             elif target >nums[mid]:
-#                 l=mid+1
-#             else:
-#                 r=mid-1
-#         return -1
