4cb2cc7c9603ac62707b3af8a31db20fd1849709
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-10 17:52:48 -0400

    Time: 256 ms (44.04%), Space: 19.2 MB (46.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:                   # Check if the grid is empty
-            return 0
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0' or grid[r][c]=='*':
-                return
-            grid[r][c] = '*'  # Mark visited
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-        
-        res = 0
+        rows=len(grid)
+        cols=len(grid[0])
+        total=0
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]=='0' or grid[r][c]=='*':
+                return 0
+            grid[r][c]='*'
+            print(r,c)
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            return 1
+        res=0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    dfs(r, c)   # Perform DFS to mark the entire island
-                    res += 1    # Increment count for the island
-        
+                    res+=dfs(r,c)
         return res
