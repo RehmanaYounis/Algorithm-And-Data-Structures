44c37aa2e9e40620c69ae50eee7bf1a595342b69
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-10 17:51:43 -0400

    Time: 234 ms (75.40%), Space: 18.9 MB (85.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,59 +1,25 @@
-class Solution(object):
-    def numIslands(self, grid):
-        rows=len(grid)
-        cols=len(grid[0])
-        visited=set()
-        total=0
-        def dfs(r,c):
-            if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visited or grid[r][c] == "0": return
-            visited.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:                   # Check if the grid is empty
+            return 0
         
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0' or grid[r][c]=='*':
+                return
+            grid[r][c] = '*'  # Mark visited
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        res = 0
         for r in range(rows):
             for c in range(cols):
-                if (r,c) not in visited and grid[r][c] =="1":
-                    total+=1
-                    dfs(r,c)
-        return total
+                if grid[r][c] == '1':
+                    dfs(r, c)   # Perform DFS to mark the entire island
+                    res += 1    # Increment count for the island
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         count=0
-#         rows=len(grid)
-#         cols=len(grid[0])
-#         visit=set()
-#         def dfs(r,c):
-#             if r<0 or r>=rows or c<0 or c>=cols or (r,c) in visit or grid[r][c]=='0':
-#                 return
-#             visit.add((r,c))
-#             dfs(r+1,c)
-#             dfs(r-1,c)
-#             dfs(r,c+1)
-#             dfs(r,c-1)
-#         for row in range(rows):
-#             for col in range(cols):
-#                 if grid[row][col]=='1' and (row,col) not in visit:
-#                     count+=1
-#                     dfs(row,col)
-#         return count
-        
+        return res
