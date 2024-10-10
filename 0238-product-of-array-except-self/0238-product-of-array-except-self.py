727429e97471310221e948ea344272db24bbb118
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-10 16:58:59 -0400

    Time: 259 ms (87.89%), Space: 25.8 MB (39.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,66 +1,23 @@
+#[1,2,3,4]
+#[1,1,2,6]
+#[24,12,4,1]
+#[24,12,8,6]
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # org = [1,2,3,4]
-        # pre = [1,1,2,6]
-        # post= [24,12,4,1]
-        #prod= [24,12,8,6]
-        
         pre=[1]*len(nums)
         post=[1]*len(nums)
         
-        preV=1
+        prev=1
         for i in range(1, len(nums)):
-            pre[i]= preV * nums[i-1]
-            preV=pre[i]
-        postV=1    
-        for i in reversed(range(len(nums)-1)):
-            post[i]=postV * nums[i+1]
-            postV=post[i]
+            pre[i]=nums[i-1]*prev
+            prev=pre[i]
         
-        for i in range(len(pre)):
+        pos=1
+        for i in reversed(range(len(nums)-1)):
+            post[i]=nums[i+1]*pos
+            pos=post[i]
+        
+        for i in range(len(nums)):
             pre[i]=pre[i]*post[i]
         return pre
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         pref=[1]*len(nums)
-#         post=[1]*len(nums)
-        
-#         for i in range(1, len(nums)):
-#             pref[i]=pref[i-1]*nums[i-1]
             
-#         for i in reversed(range(len(nums)-1)):
-#             post[i]=post[i+1]*nums[i+1]
-    
-        
-#         for i in range(len(nums)):
-#             pref[i]=pref[i]*post[i]
-#         return pref
-        
