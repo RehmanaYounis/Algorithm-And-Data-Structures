2c5932028b4eaf56a03f400f3de350bea99c1595
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-12 13:26:48 -0400

    Time: 238 ms (27.20%), Space: 20.7 MB (8.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,12 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        preMap=defaultdict(int)
-        preMap[0]=1
+        sumMap=defaultdict(int)
+        sumMap[0]=1
         curSum=0
         res=0
         for num in nums:
-            curSum +=num
+            curSum+=num
             diff=curSum-k
-            res+=preMap[diff]
-            preMap[curSum]+=1
+            res+=sumMap[diff]
+            sumMap[curSum]+=1
         return res
