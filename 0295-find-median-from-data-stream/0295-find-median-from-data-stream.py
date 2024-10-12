e0fb7f408e348e0827ebed07bd6b3ad29362ec68
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-12 17:12:19 -0400

    Time: 363 ms (75.55%), Space: 38.7 MB (33.47%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,35 +1,32 @@
 class MedianFinder:
 
     def __init__(self):
-        self.left,self.right=[],[]
-        
+        self.left=[]
+        self.right=[]
 
     def addNum(self, num: int) -> None:
-        if len(self.right) >= 1 and num>self.right[0]:
-            heapq.heappush(self.right, num)
-        else: 
-            heapq.heappush(self.left, -1*num)
-        if len(self.left)>len(self.right)+1:
-            val=-1*heapq.heappop(self.left)
-            heapq.heappush(self.right,val)
-        if len(self.right)>len(self.left)+1:
-            val=-1*heapq.heappop(self.right)
-            heapq.heappush(self.left,val)
-            
-            
-    def findMedian(self) -> float:
-        if len(self.right) > len(self.left):
-            return self.right[0]
-        elif len(self.left) > len(self.right):
-            return -1* self.left[0]
+        if len(self.right)>=1 and  num > self.right[0]:
+            heapq.heappush(self.right,num)
         else:
-            val1=self.right[0]
-            val2=-1*self.left[0]
-            return (val1 + val2)/2
-                 
+            heapq.heappush(self.left, -1* num)
+        
+        if len(self.left) > len(self.right)+1:
+            val=-1 * heapq.heappop(self.left)
+            heapq.heappush(self.right,val)
+        elif len(self.right) > len(self.left)+1:
+            val=-1 * heapq.heappop(self.right)
+            heapq.heappush(self.left,val)
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -1*self.left[0]
+        elif len(self.right)>len(self.left):
+            return self.right[0]
+        else:
+            val1=-1 * self.left[0]
+            val2= self.right[0]
+            return (val1+val2)/2
 
-      
-            
+
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
