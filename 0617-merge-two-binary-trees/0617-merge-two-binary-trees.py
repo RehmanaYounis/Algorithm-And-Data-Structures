f7586d64e938d4bb7759d252f63018bddcb32cba
Rehmana <74942033+RehmanaYounis@users.noreply.github.com>
2024-10-12 16:11:19 -0400

    Time: 69 ms (15.63%), Space: 17.1 MB (13.22%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -1,17 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def mergeTrees(self, root1, root2):
-        if not root1 and not root2:
-            return None
-        val1 =root1.val if root1 else 0
-        val2 =root2.val if root2  else 0
-        root=TreeNode(val1+val2)
-        root.left=self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None )
-        root.right=self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None )
-        return root
-       
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def dfs(tree1, tree2):
+            if not tree1 and not tree2:
+                return 
+            root=TreeNode()
+            root.val=(tree1.val if tree1 else 0) + (tree2.val if tree2 else 0)
+            root.left=dfs(tree1.left if tree1 else None, tree2.left if tree2 else None)
+            root.right=dfs(tree1.right if tree1 else None, tree2.right if tree2 else None)
+            return root
+        return dfs(root1, root2)
+    
